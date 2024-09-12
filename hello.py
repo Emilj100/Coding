be06cfd61ab8e665ecf8aa8f8bf927b45396afa9
Emilj100 <emilbondo12@gmail.com>
2024-09-12 14:22:35 +0000

    Thu, Sep 12, 2024, 4:22 PM +02:00

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 # Ask user for their name
 name = input("What's your name? ").strip().title()
 
-#
+# Split user's name into first name and last name
+name.split(" ")
 
 # Say hello to user
 print(f"hello, {name}")
