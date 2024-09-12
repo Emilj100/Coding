71c305ea5607769bca27272e96b2beaf6be5bc8d
Emilj100 <emilbondo12@gmail.com>
2024-09-12 13:08:54 +0000

    Thu, Sep 12, 2024, 1:08 PM +00:00

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 # Ask user for their name
-nama = input("What's your name? ")
+name = input("What's your name? ")
 
 # Remove whitespace from str
 name = name.strip()
 
 # Capitalize user's name
-nama = nama.capitalize()
+name = name.capitalize()
 
 # Say hello to user
 print(f"hello, {name}")
