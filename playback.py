a5664603ead7be7e697ba243b660afe3e80fc970
Emilj100 <emilbondo12@gmail.com>
2024-09-14 12:08:05 +0000

    Sat, Sep 14, 2024, 2:08 PM +02:00

--- playback.py
+++ playback.py
@@ -1,2 +1,9 @@
-def convert():
-    
+def convert(text):
+    return modified_text
+
+def main():
+    user_input = input("Hi, how are you? ")
+    result = convert(user_input)
+    print(result)
+
+main()
