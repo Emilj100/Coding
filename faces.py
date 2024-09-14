d2f689afcdded8d0b41dfa3e5c32b085713a8a70
Emilj100 <emilbondo12@gmail.com>
2024-09-14 12:44:12 +0000

    Sat, Sep 14, 2024, 2:44 PM +02:00

--- faces.py
+++ faces.py
@@ -1,10 +1,2 @@
-def convert(text):
-    text = text.replace(":)", "🙂").replace(":(", "🙁")
-    return text
-
-def main():
-    user = input("Hi, how are you? ")
-    result = convert(user)
-    print(result)
-
-main()
+# C's værdi
+C = 300000000
