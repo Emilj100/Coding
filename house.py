58a2078bcad0268a7efe0e6b0af19bc6c3a0e275
Emilj100 <emilbondo12@gmail.com>
2024-09-17 17:28:21 +0000

    Tue, Sep 17, 2024, 7:28 PM +02:00

--- house.py
+++ house.py
@@ -1,3 +1,12 @@
 name = input("What's your name? ")
 
-if name == "Harry"
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
