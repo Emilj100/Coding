fad03fda84d4fdf774c92100597aec038cbb96b7
Emilj100 <emilbondo12@gmail.com>
2024-09-17 12:54:00 +0000

    Tue, Sep 17, 2024, 2:54 PM +02:00

--- parity.py
+++ parity.py
@@ -1,6 +1,7 @@
-x = int(input("What's x?" ))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
