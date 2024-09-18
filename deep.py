923174a769bffd528690c3470305bee043fef8a2
Emilj100 <emilbondo12@gmail.com>
2024-09-18 07:24:18 +0000

    Wed, Sep 18, 2024, 9:24 AM +02:00

--- deep.py
+++ deep.py
@@ -1,6 +1,6 @@
 name = input("What is the answer to the great question of life, the universe, and everything?")
 
-name = name.strip
+name = name.strip().casefold()
 
 match name:
     case "42" | "forty Two" | "forty-two":
