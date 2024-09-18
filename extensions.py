14b6df0b59f6772682960cc0b7f1835e7605015f
Emilj100 <emilbondo12@gmail.com>
2024-09-18 20:47:03 +0000

    Wed, Sep 18, 2024, 10:47 PM +02:00

--- extensions.py
+++ extensions.py
@@ -3,7 +3,7 @@ name = input().strip().lower()
 if name.endswith(".gif"):
     print("image/gif")
 elif name.endswith(".jpg"):
-    print("image/jpg")
+    print("image/jpeg")
 elif name.endswith(".jpeg"):
     print("image/jpeg")
 elif name.endswith(".png"):
