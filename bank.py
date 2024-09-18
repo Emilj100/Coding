729b7ed9a89cb1ecfb38a62883676e307334549a
Emilj100 <emilbondo12@gmail.com>
2024-09-18 08:13:02 +0000

    Wed, Sep 18, 2024, 10:13 AM +02:00

--- bank.py
+++ bank.py
@@ -1,5 +1,11 @@
 name = input("Hi there")
 
-name = name.startswith()
+name = name.startswith("H")
 
-
+match name:
+    case "Hello" | "forty two" | "forty-two":
+        print("$0")
+    case "H":
+        print("$20")
+    case _:
+        print("$100")
