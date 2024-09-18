ba2cf40c032a7d013ff95902a2493ed2b782baaa
Emilj100 <emilbondo12@gmail.com>
2024-09-18 20:36:13 +0000

    Wed, Sep 18, 2024, 10:36 PM +02:00

--- extensions.py
+++ extensions.py
@@ -1,20 +1,4 @@
 name = input().strip().lower()
 
-match name:
-    case ".jpg":
-        print("image/jpeg")
-    case ".gif":
-        print("image/gif")
-    case ".jpeg":
-        print("image/jpeg")
-    case ".png":
-        print("image/png")
-    case ".pdf":
-        print("image/pdf")
-    case ".txt":
-        print("image/txt")
-    case ".zip":
-        print("image/zip")
-    case _:
-        print("application/octet-stream")
+
 
---
+++ test.py
@@ -0,0 +1,17 @@
+match name:
+    case ".jpg":
+        print("image/jpeg")
+    case ".gif":
+        print("image/gif")
+    case ".jpeg":
+        print("image/jpeg")
+    case ".png":
+        print("image/png")
+    case ".pdf":
+        print("image/pdf")
+    case ".txt":
+        print("image/txt")
+    case ".zip":
+        print("image/zip")
+    case _:
+        print("application/octet-stream")
