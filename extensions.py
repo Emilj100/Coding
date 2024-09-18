510528e59cc38573b8c4c2d754678a1ca7234f63
Emilj100 <emilbondo12@gmail.com>
2024-09-18 20:30:30 +0000

    Wed, Sep 18, 2024, 10:30 PM +02:00

--- extensions.py
+++ extensions.py
@@ -1,4 +1,20 @@
 name = input().strip().lower()
 
 match name:
-    case "
+    case ".jpg":
+        print("image/jpeg")
+    case ".gif":
+        print("image/gif")
+    case ".jpeg":
+        print("image/jpeg")
+    case ".png":
+        print("image/png")
+    case ".pdf":
+        print("image/pdf")
+    case ".txt":
+        print("image/txt")
+    case ".zip":
+        print("image/zip")
+    case _:
+        print()
+
