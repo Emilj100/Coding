fbe7d487925c442d6687140de891e1c502a918fe
Emilj100 <emilbondo12@gmail.com>
2024-09-18 20:44:37 +0000

    Wed, Sep 18, 2024, 10:44 PM +02:00

--- extensions.py
+++ extensions.py
@@ -1,5 +1,18 @@
 name = input().strip().lower()
 
 if name.endswith(".gif"):
-    print(im)
-
+    print("image/gif")
+elif name.endswith(".jpg"):
+    print("image/jpg")
+elif name.endswith(".jpeg"):
+    print("image/jpeg")
+elif name.endswith(".png"):
+    print("image/png")
+elif name.endswith(".pdf"):
+    print("image/pdf")
+elif name.endswith(".txt"):
+    print("image/txt")
+elif name.endswith(".zip"):
+    print("image/zip")
+else:
+    print("application/octet-stream")
