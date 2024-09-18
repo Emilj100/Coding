c8daa044b279981b15c52b441b5b9a1a3358444a
Emilj100 <emilbondo12@gmail.com>
2024-09-18 08:24:26 +0000

    Wed, Sep 18, 2024, 10:24 AM +02:00

--- bank.py
+++ bank.py
@@ -1,7 +1,16 @@
-name = input("Hi there")
+def main():
+    name = input("Hi there")
+    if funktion(name):
+        print("True")
+    else:
+        print("False")
+
 
 
 def funktion(x):
     x = x.startswith("H"):
     return True
+
+main ()
+
 
