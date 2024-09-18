dffcd03e40cc151684fb8945d9e19398344740d6
Emilj100 <emilbondo12@gmail.com>
2024-09-18 08:18:23 +0000

    Wed, Sep 18, 2024, 10:18 AM +02:00

--- bank.py
+++ bank.py
@@ -1,11 +1,7 @@
 name = input("Hi there")
 
-name = name.startswith("H")
 
-match name:
-    case "Hello" | "forty two" | "forty-two":
-        print("$0")
-    case "H":
-        print("$20")
-    case _:
-        print("$100")
+def funktion(x):
+    x = x.startswith("h"):
+    return
+
