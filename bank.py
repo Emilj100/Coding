da6e88c739a01f3804cfd04175186d9b148c3821
Emilj100 <emilbondo12@gmail.com>
2024-09-18 20:04:15 +0000

    Wed, Sep 18, 2024, 10:04 PM +02:00

--- bank.py
+++ bank.py
@@ -1,15 +1,22 @@
 def main():
     name = input("Hi there")
     if funktion(name):
-        print("True")
+        print("$20")
+    elif dollars(name):
+        print("$0")
     else:
-        print("False")
+        print("$100")
 
 
 
 def funktion(x):
     return x.startswith("H")
 
-main ()
+def dollars(z):
+    return z.endswith("ello")
+
+
+
+main()
 
 
