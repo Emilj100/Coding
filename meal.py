c17c0979a19a557fa0fb93c15f1398fc0205a401
Emilj100 <emilbondo12@gmail.com>
2024-09-19 10:26:06 +0000

    Thu, Sep 19, 2024, 12:26 PM +02:00

--- meal.py
+++ meal.py
@@ -1,34 +1,16 @@
 def main():
-    user = input("What time is it? ")
-    if breakfast(user):
-        print("breakfast time")
-    elif lunch(user):
-        print("lunch time")
-    elif dinner(user):
-        print("dinner time")
-    else:
-        print("")
+    time = input("What time is it? ")
+
+    time = convert(time)
+
+    print(time)
 
 
-def breakfast(time):
+def convert(time):
     hours, minutes = time.split(":")
-    if hours == "7" or "8":
-        return True
-    else:
-        return False
+    return float(hours) + float(minutes) / 60
 
-def lunch(time):
-    hours, minutes = time.split(":")
-    if hours == "12" or "13":
-        return True
-    else:
-        return False
-
-def dinner(time):
-    hours, minutes = time.split(":")
-    if hours == "18" or "19":
-        return True
-    else:
-        return False
+if __name__ == "__main__":
+    main()
 
 main()
