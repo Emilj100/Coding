6e8ef97ccd8fe6c9f43f607a8aeaf51f02613cff
Emilj100 <emilbondo12@gmail.com>
2024-09-19 10:41:36 +0000

    Thu, Sep 19, 2024, 12:41 PM +02:00

--- meal.py
+++ meal.py
@@ -1,9 +1,15 @@
+
 def main():
     time = input("What time is it? ")
 
     time = convert(time)
 
-    print(time)
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
 
 
 def convert(time):
@@ -12,5 +18,3 @@ def convert(time):
 
 if __name__ == "__main__":
     main()
-
-main()
