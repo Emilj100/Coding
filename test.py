3bbf714e9b09314968908e8d75e5bcf164eea585
Emilj100 <emilbondo12@gmail.com>
2024-09-19 09:51:05 +0000

    Thu, Sep 19, 2024, 11:51 AM +02:00

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 def main():
-    ...
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
 
 
 def convert(time):
@@ -7,4 +8,4 @@ def convert(time):
 
 
 if __name__ == "__main__":
-    main() 
+    main()
