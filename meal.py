b1e01399bcc2241c18566dfa98d46673de7f4df2
Emilj100 <emilbondo12@gmail.com>
2024-09-19 09:15:25 +0000

    Thu, Sep 19, 2024, 11:15 AM +02:00

--- meal.py
+++ meal.py
@@ -1,9 +1,11 @@
 def main():
-    ...
+    user = input("What time is it? ")
+    if convert(user)
 
 
 def convert(time):
-    ...
+    hours, minutes = time.split(":")
+    
 
 
 if __name__ == "__main__":
