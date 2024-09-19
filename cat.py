265fa7c0725aba9f2071b131f7f7f8abf94bce29
Emilj100 <emilbondo12@gmail.com>
2024-09-19 19:09:49 +0000

    Thu, Sep 19, 2024, 9:09 PM +02:00

--- cat.py
+++ cat.py
@@ -1,5 +1,13 @@
 def main():
-    meow(3)
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+        return n
 
 
 
