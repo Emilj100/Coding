f160bab4a3fd286256dfe37429ad8934783587a8
Emilj100 <emilbondo12@gmail.com>
2024-09-19 09:52:39 +0000

    Thu, Sep 19, 2024, 11:52 AM +02:00

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 def main():
-    if 7.0 <= time <= 8.0:
+    if 7.0 <= converttime <= 8.0:
         print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
 
 
 def convert(time):
