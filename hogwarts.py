de25b1a476ce54e8014243adef4b16496ae81780
Emilj100 <emilbondo12@gmail.com>
2024-09-20 10:39:31 +0000

    Fri, Sep 20, 2024, 12:39 PM +02:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,2 +1,6 @@
 students = ["Hermione", "Harry", "Ron", "Draco"]
 houses = ["Gryffindor", "Gryffinder", "Gryffindor", "Slytherin"]
+
+for i in range(len(students)):
+    print(students[i], houses[i])
+
