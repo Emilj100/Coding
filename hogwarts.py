8018bc3eb9e325d4009f98bc84abc7f1140cb2cf
Emilj100 <emilbondo12@gmail.com>
2024-09-21 13:04:04 +0000

    Sat, Sep 21, 2024, 3:04 PM +02:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,1 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin",
-}
-
-for student in students:
-    print(student, students[student], sep=", ")
+students
