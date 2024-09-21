95ad6adca7fb71fbbae5c2d42a74248a0294b646
Emilj100 <emilbondo12@gmail.com>
2024-09-21 17:42:02 +0000

    Sat, Sep 21, 2024, 7:42 PM +02:00

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def uppercase(case):
     for c in case:
         if c.isupper():
             print("_", end="")
-            c.lower()
+            c = c.lower()
             print(c, sep="")
         else:
             print(c, end="")
