9f85478e8c87f86f01a004b1161e7fbcecbf629f
Emilj100 <emilbondo12@gmail.com>
2024-09-21 14:51:02 +0000

    Sat, Sep 21, 2024, 4:51 PM +02:00

--- mario.py
+++ mario.py
@@ -5,12 +5,14 @@ def print_square(size):
 
 # For each row in square
     for i in range(size):
-        
+
         # For each brick in row
         for j in range(size):
 
             #Print brick
             print("#", end="")
+
+
         print()
 
 
