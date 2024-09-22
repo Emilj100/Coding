e4c4435772f0bc3fefd00bac62d679dcf1577e39
Emilj100 <emilbondo12@gmail.com>
2024-09-22 08:19:29 +0000

    Sun, Sep 22, 2024, 10:19 AM +02:00

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 def main():
     camel_case = input("Please write a name in camelCase")
     camel_case = uppercase(camel_case)
-    print(camel_case, end="", sep="")
 
 def uppercase(case):
     for c in case:
         if c.isupper():
             print("_", end="")
             c = c.lower()
-            return c
+            print(c, end="", sep="")
         else:
             print(c, end="")
 
