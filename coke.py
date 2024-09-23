c5c8092b9c061768f40d0ca0192775c59c15433b
Emilj100 <emilbondo12@gmail.com>
2024-09-23 07:14:10 +0000

    Mon, Sep 23, 2024, 9:14 AM +02:00

--- coke.py
+++ coke.py
@@ -6,7 +6,8 @@ while due > 0:
     if coin == 25 or coin == 10 or coin == 5:
         due = due - coin
         print("Amount Due:", due)
-    else
+    else:
+        print("Change Owed: ", abs(due))
     else:
         coin = int(input("Insert Coin: "))
 
