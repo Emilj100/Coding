98131745f1c2c04d4e667d3ae0dc6718c4ab7c66
Emilj100 <emilbondo12@gmail.com>
2024-09-23 17:36:19 +0000

    Mon, Sep 23, 2024, 7:36 PM +02:00

--- plates.py
+++ plates.py
@@ -4,27 +4,27 @@ def main():
         print("Valid")
     else:
         print("Invalid")
-
+# Sikre at de 2 første tegn er bogstaver
+      #Sikre at der maks er 6 tegn i nummberpladen
 
 def is_valid(s):
-        # Sikre at de 2 første tegn er bogstaver
     if not s[0:2].isalpha():
-        #Sikre at der maks er 6 tegn i nummberpladen
-        if len(s) > 6 or len(s) < 2:
-                return False
-    else:
+        return False
+    if len(s) > 6 or len(s) < 2:
          return False
+
+
 
     found_digit = False
 
     for c in s:
-         if c.isdigit():
-              if not found_digit:
-                   found_digit = True
-                   if c == "0":
-                        return False
-         elif found_digit and c.isalpha():
-              return False
+        if c.isdigit():
+            if not found_digit:
+                found_digit = True
+                if c == "0":
+                    return False
+        elif found_digit and c.isalpha():
+            return False
     return True
 
 
