18e48b5dc418da1b27dce206cb32cbdd6f136bfd
Emilj100 <emilbondo12@gmail.com>
2024-09-23 11:37:39 +0000

    Mon, Sep 23, 2024, 1:37 PM +02:00

--- plates.py
+++ plates.py
@@ -10,9 +10,9 @@ def is_valid(s):
     for c in s:
         found_digit = c.isdigit()
         if found_digit == 0:
-            return False
+            break
         elif found_digit.isalpha():
-            return False
+            return
 
 
 
