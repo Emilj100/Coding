a30e6124d2fd60efb4445b8465c635c320a72e1b
Emilj100 <emilbondo12@gmail.com>
2024-09-23 06:47:27 +0000

    Mon, Sep 23, 2024, 8:47 AM +02:00

--- coke.py
+++ coke.py
@@ -5,11 +5,12 @@ coin = int(input("Insert Coin: "))
 due = 50
 
 
-while due >= 0:
+while due > 0:
     if coin == 25 or coin == 10 or coin == 5:
-        print("Amount Due:", due - coin)
         due = due - coin
+        print("Amount Due:", due)
         coin = int(input("Insert Coin: "))
+        due = due - coin
     else:
         print("Prøv igen")
         break
