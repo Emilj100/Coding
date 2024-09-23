eec8916eaee0de51b0895de26efd335cad4b76ad
Emilj100 <emilbondo12@gmail.com>
2024-09-23 07:05:05 +0000

    Mon, Sep 23, 2024, 9:05 AM +02:00

--- coke.py
+++ coke.py
@@ -1,7 +1,3 @@
-# Beder brugeren om at indsætte et mønt
-
-
-# Viser hvor meget brugeren skylder (Starter med 50 cents)
 due = 50
 
 
@@ -10,11 +6,13 @@ while due > 0:
     if coin == 25 or coin == 10 or coin == 5:
         due = due - coin
         print("Amount Due:", due)
-    elif due == 0:
-        due + coin
-        print("Change Owed: ", due)
     else:
         coin = int(input("Insert Coin: "))
+
+while due == 0:
+    if due == 0:
+        due + coin
+        print("Change Owed: ", due)
 
 
 
