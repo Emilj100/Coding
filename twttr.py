495b74b26b960746b2eccf31dbdf4cb7f55ea52b
Emilj100 <emilbondo12@gmail.com>
2024-09-23 08:31:13 +0000

    Mon, Sep 23, 2024, 10:31 AM +02:00

--- twttr.py
+++ twttr.py
@@ -1,6 +1,10 @@
 def main():
     full_version = input("Input: ")
     new_version = short(full_version)
+    print(new_version)
 
 def short(short_version):
+    for c in short_version:
+    print(c, end="")
 
+main()
