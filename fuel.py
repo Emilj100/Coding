65793f20be887e1ff4169d8f2816559781c9d5fb
Emilj100 <emilbondo12@gmail.com>
2024-09-24 13:32:54 +0000

    Tue, Sep 24, 2024, 3:32 PM +02:00

--- fuel.py
+++ fuel.py
@@ -1,5 +1,7 @@
 try:
-    x, y = int(input("Fraction: ")).split("/")
+    x, y = (input("Fraction: ").split("/"))
+    x = int(x)
+    y = int(y)
 except ValueError:
     ...
 except ZeroDivisionError:
