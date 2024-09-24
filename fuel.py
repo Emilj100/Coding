eda6e5fe233073dc9997d36d0f537c8ebb1440e0
Emilj100 <emilbondo12@gmail.com>
2024-09-24 14:25:51 +0000

    Tue, Sep 24, 2024, 4:25 PM +02:00

--- fuel.py
+++ fuel.py
@@ -1,9 +1,13 @@
-try:
-    x, y = (input("Fraction: ").split("/"))
-    x = int(x)
-    y = int(y)
+while True:
+    try:
+        x, y = (input("Fraction: ").split("/"))
+        x = int(x)
+        y = int(y)
+        procent = round((x / y) * 100)
 
-except ValueError:
-    ...
-except ZeroDivisionError:
-    ...
+    except ValueError:
+        ...
+    except ZeroDivisionError:
+        ...
+
+    print(procent, "%", sep="")
