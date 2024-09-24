ff2b5be3b4bc9cba28872d4589279760be4b59be
Emilj100 <emilbondo12@gmail.com>
2024-09-24 14:15:05 +0000

    Tue, Sep 24, 2024, 4:15 PM +02:00

--- fuel.py
+++ fuel.py
@@ -1,20 +1,10 @@
 try:
-    z = (input("Fraction: "))
-    x, y = z.split("/")
+    x, y = (input("Fraction: ").split("/"))
     x = int(x)
     y = int(y)
-
+    
 except ValueError:
     ...
 except ZeroDivisionError:
     ...
-
-fuel = {
-    "1/4": "25%",
-    "2/4": "50%",
-    "3/4": "75",
-    "4/4": "F",
-}
-
-if z in fuel:
-    print(fuel[z])
+print(y)
