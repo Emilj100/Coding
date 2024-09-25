d9e32b7a6277a972bebdb933f3339d99848eca5a
Emilj100 <emilbondo12@gmail.com>
2024-09-25 16:05:06 +0000

    Wed, Sep 25, 2024, 6:05 PM +02:00

--- outdated.py
+++ outdated.py
@@ -15,7 +15,24 @@ month = {
 
 while True:
     user_input = input("Date: ")
-    if user_input.find("/"):
-        print("ja")
+    if user_input.find("/") != -1:
+
+
+
+    .replace(",", "").title().replace("/", " ").strip().split(" ")
+
+
+
+    if x in month:
+        x = month[x]
+    if y in month:
+        continue
+    x = int(x)
+    y = int(y)
+    if x >= 13:
+        continue
+    if y >= 32:
+        continue
     else:
-        print("nej")
+        print(f"{z}-{x:02}-{y:02}")
+        break
