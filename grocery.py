4df1d71d4d2effb67a9afe52138c1ba4203c46e4
Emilj100 <emilbondo12@gmail.com>
2024-09-25 12:04:44 +0000

    Wed, Sep 25, 2024, 2:04 PM +02:00

--- grocery.py
+++ grocery.py
@@ -5,13 +5,13 @@ dict = {
 
 while True:
     try:
-        item = input().upper()
+        item = input().upper().strip()
         if item not in dict:
             dict[item] = 1
         elif item in dict:
             dict[item] += 1
     except EOFError:
         for item in sorted(dict):
-            print(dict[item], item)
-    break
+            print(dict[item], item, end="")
+        break
 
