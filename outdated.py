0fe97bcdce640f2ea586edead992152956d8d4b4
Emilj100 <emilbondo12@gmail.com>
2024-09-25 13:49:36 +0000

    Wed, Sep 25, 2024, 3:49 PM +02:00

--- outdated.py
+++ outdated.py
@@ -1,17 +1,22 @@
-x, y, z = input("Date: ").replace("/", " ").split(" ")
+x, y, z = input("Date: ").title().replace("/", " ").split(" ")
 
 
 month = {
     "January": 1,
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12,
 }
+
+if x in month:
+    x = month[x]
+
+print(x)
