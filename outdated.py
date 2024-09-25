a221412a8a97dc20c2e413adf2fc4def7d546cec
Emilj100 <emilbondo12@gmail.com>
2024-09-25 16:17:07 +0000

    Wed, Sep 25, 2024, 6:17 PM +02:00

--- outdated.py
+++ outdated.py
@@ -26,28 +26,14 @@ while True:
         if dd >= 32:
             continue
         else:
-            print(f"{z}-{x:02}-{y:02}")
+            print(f"{yyyy}-{mm:02}-{dd:02}")
             break
-    elif
-
-
-
-
-
-    .replace(",", "").title().replace("/", " ").strip().split(" ")
-
-
-
-    if x in month:
-        x = month[x]
-    if y in month:
-        continue
-    x = int(x)
-    y = int(y)
-    if x >= 13:
-        continue
-    if y >= 32:
-        continue
     else:
-        print(f"{z}-{x:02}-{y:02}")
-        break
+        mm, dd, yyyy = user_input.replace(",", "").split(" ")
+        if mm in month:
+            mm = month{mm}
+        if dd in month:
+            continue
+        else:
+            print(f"{yyyy}-{mm:02}-{dd:02}")
+            break
