9be65039c9f334e9c89e424b046369fa53e75ae9
Emilj100 <emilbondo12@gmail.com>
2024-09-25 11:52:33 +0000

    Wed, Sep 25, 2024, 1:52 PM +02:00

--- grocery.py
+++ grocery.py
@@ -5,12 +5,12 @@ dict = {
 
 while True:
     try:
-        item = input()
+        item = input().
         if item not in dict:
             dict[item] = 1
         elif item in dict:
             dict[item] += 1
     except EOFError:
         for item in dict:
-            print(item, dict[item])
+            print(dict[item], item)
 
