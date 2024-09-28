90807dfa1d6f6366135893147bb35a2962526c6a
Emilj100 <emilbondo12@gmail.com>
2024-09-28 19:42:46 +0000

    Sat, Sep 28, 2024, 9:42 PM +02:00

--- adieu.py
+++ adieu.py
@@ -1,14 +1,17 @@
 import inflect
 
+dict = [
 
+]
 
 
 while True:
     p = inflect.engine()
     try:
         user_input = input("Name: ")
+        dict[user_input] = 1
 
 
     except EOFError:
-        mylist = p.join((user_input))
+        mylist = p.join((dict))
         print(mylist)
