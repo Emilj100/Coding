f802dafccbee266135c60c42efa3bd564c7878b6
Emilj100 <emilbondo12@gmail.com>
2024-09-28 12:41:22 +0000

    Sat, Sep 28, 2024, 2:41 PM +02:00

--- figlet.py
+++ figlet.py
@@ -1,10 +1,13 @@
 import sys
 from pyfiglet import Figlet
 
-
+def main():
 user_input = input("Output: ")
+f = font()
 
-if len(sys.argv) == 2:
-    f = Figlet(font='slant')
+def font(f):
+    if len(sys.argv) == 2:
+        f = Figlet(font='slant')
+        return f
 
 print(f.renderText(user_input))
