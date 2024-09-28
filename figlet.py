faab3d2dde8e3499a8a0ed7a8eaef220aa1e7130
Emilj100 <emilbondo12@gmail.com>
2024-09-28 13:24:14 +0000

    Sat, Sep 28, 2024, 3:24 PM +02:00

--- figlet.py
+++ figlet.py
@@ -2,10 +2,14 @@ import sys
 from pyfiglet import Figlet
 from random import choice
 
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
     user_input = input("Output: ")
 
-    
+    random_font = choice(["slant", "rectangles", "alphabet", "acrobatic", "alligator2"])
+
+    f = Figlet(font=random_font)
+
+    print(f.renderText(user_input))
 
 
 elif len(sys.argv) == 3:
