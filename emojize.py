ac597d2a2b7753d5043992aadbbadbcfce9e76fd
Emilj100 <emilbondo12@gmail.com>
2024-09-28 11:06:02 +0000

    Sat, Sep 28, 2024, 1:06 PM +02:00

--- emojize.py
+++ emojize.py
@@ -1,19 +1,5 @@
-emoji_dict = {
-    ":thumbsup:": "👍",
-    ":thumbs_up:": "👍",
-    ":grinning:": "😀",
-    ":smile:": "😄",
-    ":heart:": "❤️",
-    ":sun:": "☀️"
-    # Tilføj flere emojis og deres aliaser efter behov
-}
+import emoji
 
-# Få input fra brugeren
 user_input = input("Input: ")
 
-# Gå igennem hver nøgle i ordbogen og erstat aliaser med den tilsvarende emoji
-for code, emj in emoji_dict.items():
-    user_input = user_input.replace(code, emj)
-
-# Udskriv resultatet med de erstattede emojis
-print(f"Output: {user_input}")
+print(emoji.emojize(user_input, language="alias"))
