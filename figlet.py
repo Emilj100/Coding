38397bea71f73495a59ade3b124892adb784e39b
Emilj100 <emilbondo12@gmail.com>
2024-09-28 13:03:37 +0000

    Sat, Sep 28, 2024, 3:03 PM +02:00

--- figlet.py
+++ figlet.py
@@ -1,9 +1,9 @@
 import sys
 from pyfiglet import Figlet
 
+if len(sys.argv) > 2:
+    user_input = input("Output: ")
 
-user_input = input("Output: ")
+    f = Figlet(font=sys.argv[2])
 
-f = Figlet(font=sys.argv[2])
-
-print(f.renderText(user_input))
+    print(f.renderText(user_input))
