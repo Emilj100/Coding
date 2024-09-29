735019fef0ca108f198a61f2534923ed2280b8d0
Emilj100 <emilbondo12@gmail.com>
2024-09-29 15:13:22 +0000

    Sun, Sep 29, 2024, 5:13 PM +02:00

--- bitcoin.py
+++ bitcoin.py
@@ -1,6 +1,8 @@
 import requests
+import json
+import sys
 
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json)
 
-response = reguests.get(https://api.coindesk.com/v1/bpi/currentprice.json)
-
+print(response.json())
 
