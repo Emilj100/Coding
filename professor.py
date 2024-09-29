30c57c6ef68acaa12015c7ca5b21d9afd40d6e70
Emilj100 <emilbondo12@gmail.com>
2024-09-29 09:55:09 +0000

    Sun, Sep 29, 2024, 11:55 AM +02:00

--- professor.py
+++ professor.py
@@ -3,16 +3,22 @@ import random
 
 def main():
     level = get_level()
-    random_integer = generate_integer(level)
+    print(level)
 
 
 
 def get_level():
-    ...
+    while True:
+        user_input = int(input("Level: "))
+        if not user_input == 1 or user_input == 2 or user_input == 3:
+            continue
+        else:
+            return user_input
+        
 
 
-def generate_integer(level):
-    ...
+
+
 
 
 if __name__ == "__main__":
