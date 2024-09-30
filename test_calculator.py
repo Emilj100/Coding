5189e5708b1ca3058cdfbd84cda959f9d6fc7bbb
Emilj100 <emilbondo12@gmail.com>
2024-09-30 12:48:15 +0000

    Mon, Sep 30, 2024, 2:48 PM +02:00

--- test_calculator.py
+++ test_calculator.py
@@ -16,11 +16,11 @@ def test_square():
     try:
         assert square(-2) == 9
     except AssertionError:
-        print("3 squared was not 9")
+        print("-2 squared was not 9")
     try:
         assert square(-3) == 9
     except AssertionError:
-        print("3 squared was not 9")
+        print("-3 squared was not 9")
 
 
 
