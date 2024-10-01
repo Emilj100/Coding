03b0dc503dab22d4885c0b684f6822b607a3a42e
Emilj100 <emilbondo12@gmail.com>
2024-10-01 13:45:30 +0000

    Tue, Oct 1, 2024, 3:45 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,21 +3,6 @@ def main():
     new_version = short(full_version)
     print(new_version)
 
-def short(short_version):
-    result = ""
-    for c in short_version:
-        if c not in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
-            result += c
-
-    return result
-
-main()
-
-def main():
-    full_version = input("Input: ")
-    new_version = short(full_version)
-    print(new_version)
-
 
 def shorten(word):
     result = ""
