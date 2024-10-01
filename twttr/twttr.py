7fc98b49678969b0495de7db4929540afbbfca54
Emilj100 <emilbondo12@gmail.com>
2024-10-01 13:29:56 +0000

    Tue, Oct 1, 2024, 3:29 PM +02:00

---
+++ twttr/twttr.py
@@ -0,0 +1,34 @@
+def main():
+    full_version = input("Input: ")
+    new_version = short(full_version)
+    print(new_version)
+
+def short(short_version):
+    result = ""
+    for c in short_version:
+        if c not in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
+            result += c
+
+    return result
+
+main()
+
+def main():
+    full_version = input("Input: ")
+    new_version = short(full_version)
+    print(new_version)
+
+
+def shorten(word):
+    result = ""
+    for c in short_version:
+        if c not in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
+            result += c
+
+    return result
+
+
+
+
+if __name__ == "__main__":
+    main()
