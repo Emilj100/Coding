2831830adebfc72c8f6b62f707493a39bdaabae0
Emilj100 <emilbondo12@gmail.com>
2024-10-02 09:12:32 +0000

    Wed, Oct 2, 2024, 11:12 AM +02:00

--- test_bank.py
+++ test_bank.py
@@ -3,6 +3,7 @@ from bank import value
 def test_value_uppercase():
     assert value("HELLO") == "$0"
     assert value("HEY") == "$20"
+    assert value("Do you need help?")
 
 
 def test_value_lowercase():
@@ -17,4 +18,4 @@ def test_value_punctuation():
 def test_value_number():
     assert value("hello, give me $100") == "$0"
     assert value("hey, give me $100") == "$20"
-    
+
