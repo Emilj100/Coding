45e2eaa9af2dce5f8404b04fc743aa2efcf94ba3
Emilj100 <emilbondo12@gmail.com>
2024-10-02 12:04:04 +0000

    Wed, Oct 2, 2024, 2:04 PM +02:00

--- test_plates.py
+++ test_plates.py
@@ -9,9 +9,18 @@ def test_is_valid_punctuation():
     assert is_valid("udb.21") == False
 
 def test_is_valid_uppercase():
-    assert is_valid("HEJ123") == False
+    assert is_valid("HEJ023") == False
 
 def test_is_valid_lowercase():
-    assert is_valid("hej123") == False
+    assert is_valid("hej023") == False
 
+# Denne del tester for ting der gerne skulle returnere True korrekt
 
+def test_is_valid_many_digits1():
+    assert is_valid("abc123") == True
+
+def test_is_valid_uppercase1():
+    assert is_valid("ABCDEF") == True
+
+def test_is_valid_lowercase1():
+    assert is_valid("ab12") == True
