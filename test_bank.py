4226062bd43cf22afd6705d118c28a4e85e71f18
Emilj100 <emilbondo12@gmail.com>
2024-10-02 11:01:59 +0000

    Wed, Oct 2, 2024, 1:01 PM +02:00

--- test_bank.py
+++ test_bank.py
@@ -1,20 +1,24 @@
 from bank import value
 
-def test_hello():
-    assert value('hello') == 0
-    assert value('hello, world') == 0
+def test_value_uppercase():
+    assert value("HELLO") == 0
+    assert value("HEY") == 20
+    assert value("DO YOU NEED HELP?") == 100
 
 
-def test_h():
-    assert value('hey') == 20
-    assert value('how ya doin') == 20
-    assert value('hola habibi') == 20
-    assert value('hel, lo') == 20
+def test_value_lowercase():
+    assert value("hello") == 0
+    assert value("hey") == 20
+    assert value("do you need help?") == 100
 
-def test_noH():
-    assert value('whats up, buddy') == 100
-    assert value('what the he!@') == 100
+def test_value_punctuation():
+    assert value("hello, how are you?") == 0
+    assert value("hey, how are you?") == 20
+    assert value("My friend, do you need help?") == 100
 
-def test_capital():
-    assert value('HELLO') == 0
-    assert value('Hey') == 20
+
+def test_value_number():
+    assert value("hello, give me $100") == 0
+    assert value("hey, give me $100") == 20
+    assert value("give me $100 now") == 100
+
