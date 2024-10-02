b46cdba73cd9d147c5707e303b45f8fdc40d679d
Emilj100 <emilbondo12@gmail.com>
2024-10-02 06:58:30 +0000

    Wed, Oct 2, 2024, 8:58 AM +02:00

--- test_twttr.py
+++ test_twttr.py
@@ -1,8 +1,5 @@
 from twttr import shorten
 
 def test_shorten():
-    assert shorten("David") == "Dvd"
-
-def test_shorten2():
-    assert shorten("Twitter") == "Twitter"
+    assert shorten("DAvid") == "Dvd"
 
