a16c87e6901b5877b1bf56e537333207041e4e54
Emilj100 <emilbondo12@gmail.com>
2024-10-02 17:55:19 +0000

    Wed, Oct 2, 2024, 7:55 PM +02:00

--- test_fuel.py
+++ test_fuel.py
@@ -5,11 +5,35 @@ def test_convert_inputs():
     assert convert("3/4") == 75
     assert convert("1/4") == 25
 
-def test_convert_symbols()
+def test_convert_symbols():
     assert convert("37.88") == 42
     assert convert("3-4") == 75
     assert convert("1*4") == 25
 
-def test_convert_
+def test_convert_str():
+    assert convert(37/88) == 42
+    assert convert(3/4) == 75
+    assert convert(1/4) == 25
 
+def test_convert_x():
+    assert convert(88/37) == 42
+    assert convert(4/3) == 75
+    assert convert(4/1) == 25
+
+def test_convert_100():
+    assert convert(75/125) == 100
+    assert convert(-25/-30) == 50
+    assert convert(25/0) == 25
+
+def test_gauge_E():
+    assert gauge(1/100) == "E"
+    assert gauge(100/1) == "E"
+
+def test_gauge_F():
+    assert gauge(99/100) == "F"
+    assert gauge(100/100) == "F"
+    assert gauge(100/99) == "F"
+
+def test_gauge_percentage():
+    assert gauge(3/4) == "25%"
 
