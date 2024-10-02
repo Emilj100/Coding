cf98bbe4ab23c9d5d4808fac6510174dee3966bd
Emilj100 <emilbondo12@gmail.com>
2024-10-02 16:25:33 +0000

    Wed, Oct 2, 2024, 6:25 PM +02:00

--- fuel.py
+++ fuel.py
@@ -1,9 +1,25 @@
 def main():
-    ...
+    percentage = convert()
+    
+
+
 
 
 def convert(fraction):
-    ...
+    x, y = (input("Fraction: ").split("/"))
+    x = int(x)
+    y = int(y)
+    if x or y > 100:
+        raise ValueError
+    elif x or y < 0:
+        raise ValueError
+    elif x > y:
+        raise ValueError
+    elif y == 0:
+        raise ZeroDivisionError
+    else:
+        percentage = round((x / y) * 100)
+        return percentage
 
 
 def gauge(percentage):
