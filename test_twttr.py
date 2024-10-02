437f4702afae1194b1c72fce0222eabaeb9b1b7e
Emilj100 <emilbondo12@gmail.com>
2024-10-02 07:34:36 +0000

    Wed, Oct 2, 2024, 9:34 AM +02:00

--- test_twttr.py
+++ test_twttr.py
@@ -3,3 +3,7 @@ from twttr import shorten
 def test_shorten():
     assert shorten("DAvid") == "Dvd"
 
+def test_shorten_numbers():
+    assert shorten("1da2k") == "1d2k"
+
+def test_shorten_puncuation("davi.")
