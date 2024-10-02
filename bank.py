c203ccaac571a9a10726c78c7ede0cb912d1c6bc
Emilj100 <emilbondo12@gmail.com>
2024-10-02 10:36:44 +0000

    Wed, Oct 2, 2024, 12:36 PM +02:00

--- bank.py
+++ bank.py
@@ -1,15 +1,15 @@
 def main():
     name = input("Hi there ").lower()
-    print(value(name))
+    print("$", value(name), sep="")
 
 
 def value(greeting):
     if greeting.lower().startswith("hello"):
-        return f"$0"
+        return 0
     elif greeting.lower().startswith("h"):
-        return f"$20"
+        return 20
     else:
-        return f"$100"
+        return 100
 
 
 if __name__ == "__main__":
