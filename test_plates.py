f137af87cd3d416aae1077fdf3e7561009315bfb
Emilj100 <emilbondo12@gmail.com>
2024-10-02 12:01:03 +0000

    Wed, Oct 2, 2024, 2:01 PM +02:00

--- test_plates.py
+++ test_plates.py
@@ -1,8 +1,9 @@
 from plates import is_valid
 
+# Denne del tester om for ting der gerne skulle returnere False korrekt
+
 def test_is_valid_many_digits():
     assert is_valid("test123") == False
-
 
 def test_is_valid_punctuation():
     assert is_valid("udb.21") == False
@@ -12,3 +13,5 @@ def test_is_valid_uppercase():
 
 def test_is_valid_lowercase():
     assert is_valid("hej123") == False
+
+
