f2dca5f490e1d5fb6ef0ba68009ed4cb93ffb5a0
Emilj100 <emilbondo12@gmail.com>
2024-10-03 15:13:56 +0000

    Thu, Oct 3, 2024, 5:13 PM +02:00

--- students.py
+++ students.py
@@ -9,5 +9,5 @@ with open("students.csv") as file:
 def get_house(student):
     return student["house"]
 
-for student in sorted(students, key=get_house, reverse=True):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
