b844e5cfa8c81d23414f30b30106acee444a38fa
Emilj100 <emilbondo12@gmail.com>
2024-10-03 12:12:45 +0000

    Thu, Oct 3, 2024, 2:12 PM +02:00

--- name.py
+++ name.py
@@ -1,3 +1,8 @@
+names = []
+
 with open("names.txt") as file:
-    for line in sorted(file):
-        print("hello,", line.rstrip())
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
