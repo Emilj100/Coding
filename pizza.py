c721f78e6ecc98977c25acdd57100e95e5491c42
Emilj100 <emilbondo12@gmail.com>
2024-10-04 18:35:14 +0000

    Fri, Oct 4, 2024, 8:35 PM +02:00

---
+++ pizza.py
@@ -0,0 +1,30 @@
+import sys
+
+user_input = sys.argv[1]
+
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+elif len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+
+elif not user_input.endswith(".csv"):
+    sys.exit("Not a CSV file")
+
+try:
+
+
+except FileNotFoundError:
+    sys.exit("File does not exist")
+
+try:
+    with open(user_input) as file:
+        for row in file:
+            row = row.lstrip()
+            if row.lstrip() and not row.startswith("#"):
+                lines += 1
+
+except FileNotFoundError:
+    sys.exit("File does not exist")
+
+print(lines)
