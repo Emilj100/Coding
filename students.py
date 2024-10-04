afb0b23338767fe57e0c36b35eb5d072db1b984c
Emilj100 <emilbondo12@gmail.com>
2024-10-04 08:05:45 +0000

    Fri, Oct 4, 2024, 10:05 AM +02:00

--- students.py
+++ students.py
@@ -1,10 +1,9 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, home = line.rstrip().split(",")
-        student = {"name": name, "home": home}
-        students.append(student)
+    reader = csv.reader(file)
 
 
 for student in sorted(students, key=lambda student: student["name"]):
