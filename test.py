504505f50ee777d84c1523d26c70faedf652d8a5
Emilj100 <emilbondo12@gmail.com>
2024-10-04 17:54:00 +0000

    Fri, Oct 4, 2024, 7:54 PM +02:00

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-user_input = input("Hejsa dette er en test ")
+from sys import argv, exit
 
-# test
-print(user_input)
+if len(argv) != 2:
+    if len(argv) < 2:
+        exit('Too few command-line arguments')
+    else:
+        exit('Too mant command-line arguments')
 
-print(user_input)
+filename = argv[1]
+
+dot = filename.find('.')
+if filename[dot:] != '.py':
+    exit('Not a Python file')
+
+lines = 0
+
+try:
+    with open(filename,'r') as file:
+        for line in file:
+            if line.isspace():
+                continue
+            if line.strip().startswith('#'):
+                continue
+            lines +=1
+except FileNotFoundError:
+    exit('File does not exist')
+
+print(lines)
