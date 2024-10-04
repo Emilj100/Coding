fc2dcc36f21cfe69d70525b2bb24a8efb0101927
Emilj100 <emilbondo12@gmail.com>
2024-10-04 15:25:02 +0000

    Fri, Oct 4, 2024, 5:25 PM +02:00

--- lines.py
+++ lines.py
@@ -1,11 +1,13 @@
 import sys
 
+user_input = sys.argv[1]
+
 if len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
 elif len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 
-elif not sys.argv.endswith(".py"):
+elif not user_input.endswith(".py"):
     sys.exit("Not a Python file")
 
