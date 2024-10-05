b3605c17b0d5ba03b53529712208bd05bd899858
Emilj100 <emilbondo12@gmail.com>
2024-10-05 15:41:45 +0000

    Sat, Oct 5, 2024, 5:41 PM +02:00

--- scourgify.py
+++ scourgify.py
@@ -2,11 +2,14 @@ import sys
 import tabulate
 import csv
 
+
 def main():
+user_input = sys.argv[1:]
+part_1 = sys.argv[1]
+part_2 = sys.argv[2]
 
 
-
-def correct_input(user_input):
+def correct_input():
     if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 
@@ -18,13 +21,5 @@ def correct_input(user_input):
 
 
 
-user_input = sys.argv[1]
 
-if len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
 
-elif len(sys.argv) == 1:
-    sys.exit("Too few command-line arguments")
-
-elif not user_input.endswith(".csv"):
-    sys.exit("Not a CSV file")
