2a9df345d42404cf6fa0f1c8f61131abb642b3bc
Emilj100 <emilbondo12@gmail.com>
2024-10-05 12:38:38 +0000

    Sat, Oct 5, 2024, 2:38 PM +02:00

--- test.py
+++ test.py
@@ -1,38 +1,39 @@
-import sys
-import tabulate
+from sys import argv, exit
+from tabulate import tabulate
 import csv
 
-user_input = sys.argv[1]
+def main():
+    filename = get_filename(argv)
+    check_csv(filename)
 
-if len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
-
-elif len(sys.argv) == 1:
-    sys.exit("Too few command-line arguments")
-
-elif not user_input.endswith(".csv"):
-    sys.exit("Not a CSV file")
-
-data = []
-
-try:
-    with open(user_input) as file:
-        table = csv.DictReader(file, fieldnames=["Regular Pizza", "Small", "Large"])
-        for row in table:
-            data.append(list(row.values()))
-
-
-    print(tabulate.tabulate(data, tablefmt="grid"))
-
-
-
-except FileNotFoundError:
-    sys.exit("File does not exist")
-
-
-
-        reader = csv.reader(file)
+    try:
+        with open(filename, 'r') as file:
+            reader = csv.reader(file)
             table = []
             headers = list(next(reader))
             for row in reader:
                 table.append([row[0], row[1], row[2]])
+
+        # Move print statement outside the loop
+        print(tabulate(table, headers, tablefmt="grid"))
+
+    except FileNotFoundError:
+        exit('File does not exist')
+    except Exception as arg:
+        exit(arg)
+
+def get_filename(arguments):
+    if len(arguments) != 2:
+        if len(arguments) < 2:
+            exit('Too few command-line arguments')
+        else:
+            exit('Too many command-line arguments')
+    return arguments[1]
+
+def check_csv(filename):
+    dot = filename.find('.')
+    if filename[dot:] != '.csv':
+        exit('Not a CSV file')
+
+if __name__ == '__main__':
+    main()
