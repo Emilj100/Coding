56292bd5f796b6767120a206411318bc12c74222
Emilj100 <emilbondo12@gmail.com>
2024-10-05 15:39:08 +0000

    Sat, Oct 5, 2024, 5:39 PM +02:00

--- scourgify.py
+++ scourgify.py
@@ -7,5 +7,24 @@ def main():
 
 
 def correct_input(user_input):
-    
+    if len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
 
+    elif len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+
+
+
+
+
+
+user_input = sys.argv[1]
+
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+elif len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+
+elif not user_input.endswith(".csv"):
+    sys.exit("Not a CSV file")
