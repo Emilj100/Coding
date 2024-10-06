d3f9b0768b180171880c53c2ccefe87eb2608bed
Emilj100 <emilbondo12@gmail.com>
2024-10-06 12:35:42 +0000

    Sun, Oct 6, 2024, 2:35 PM +02:00

--- shirt.py
+++ shirt.py
@@ -4,9 +4,10 @@ import csv
 
 
 user_input = sys.argv[1:]
-
-
-
+old_file = sys.argv[1].lower()
+new_file = sys.argv[2].lower()
+file_name1, old_file_extension = old_file.split(".")
+file_name2, new_file_extension = new_file.split(".")
 
 if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
@@ -14,16 +15,10 @@ if len(sys.argv) > 3:
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 
-old_file = sys.argv[1].lower()
-new_file = sys.argv[2].lower()
-
-if not old_file.endswith((".jpg", "jpeg", "png")) or not new_file.endswith((".jpg", "jpeg", "png")):
+elif not old_file.endswith((".jpg", "jpeg", "png")) or not new_file.endswith((".jpg", "jpeg", "png")):
     sys.exit("Invalid output")
 
-file_name1, old_file_extension = old_file.split(".")
-file_name2, new_file_extension = new_file.split(".")
-
-if not new_file_extension == old_file_extension:
+elif not new_file_extension == old_file_extension:
     sys.exit("Input and output have different extensions")
 
 
