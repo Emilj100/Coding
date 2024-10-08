ab6455183c90fb3144a7cc53c5194e5a0e28c9e4
Emilj100 <emilbondo12@gmail.com>
2024-10-08 13:50:49 +0000

    Tue, Oct 8, 2024, 3:50 PM +02:00

--- format.py
+++ format.py
@@ -2,4 +2,8 @@ import re
 
 name = input("What's your name? ").strip()
 
-matches = re.search(r"^.+, .+$", name)
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last, first = matches.groups()
+    name = f"{first} {last}"
+print(f"hello, {name}")
