a5d2e93c404b66a947e01b7613d3db7ca56db61a
Emilj100 <emilbondo12@gmail.com>
2024-10-08 07:26:38 +0000

    Tue, Oct 8, 2024, 9:26 AM +02:00

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.(com|edu|gov|$", email):
+if re.search(r"^\w+@\w+\.(com|edu|gov|net)$", email):
     print("Valid")
 else:
     print("Invalid")
