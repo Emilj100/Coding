3b477c059ee7e7b03da96546a2089a1da59afcd5
Emilj100 <emilbondo12@gmail.com>
2024-10-09 13:46:24 +0000

    Wed, Oct 9, 2024, 3:46 PM +02:00

--- numb3rs.py
+++ numb3rs.py
@@ -8,17 +8,17 @@ def main():
 
 def validate(ip):
     if ip := re.search(r"^(([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+))$", ip):
-        int(ip.group(2))
-        int(ip.group(3))
-        int(ip.group(4))
-        int(ip.group(5))
-        if ip.group(2) > 0 and ip.group(2) < 255:
-            if ip.group(3) > 0 and ip.group(3) < 255:
-                if ip.group(4) > 0 and ip.group(4) < 255:
-                    if ip.group(5) > 0 and ip.group(5) < 255:
+        ip_2 = int(ip.group(2))
+        ip_3 = int(ip.group(3))
+        ip_4 = int(ip.group(4))
+        ip_5 = int(ip.group(5))
+        if ip_2 >= 0 and ip_2 <= 255:
+            if ip_3 >= 0 and ip_3 <= 255:
+                if ip_4 >= 0 and ip_4 <= 255:
+                    if ip_5 >= 0 and ip_5 <= 255:
                         return True
-    else:
-        return False
+        else:
+            return False
 
 
 if __name__ == "__main__":
