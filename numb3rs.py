5f2608e49e9c1cddfbdb0f370b905fdfb2f611f8
Emilj100 <emilbondo12@gmail.com>
2024-10-09 13:27:41 +0000

    Wed, Oct 9, 2024, 3:27 PM +02:00

--- numb3rs.py
+++ numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    if ip := re.search(r"^([2]+\.[5]+\.[6]+\.[9]+)$", ip):
+    if ip := re.search(r"^([0-250]\.[0-250]\.[0-250]\.[0-250])$", ip):
         return True
     else:
         return False
