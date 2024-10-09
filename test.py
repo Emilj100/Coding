1e9aacbe23dddb8a05795eed2605c74a07eda8bc
Emilj100 <emilbondo12@gmail.com>
2024-10-09 19:01:28 +0000

    Wed, Oct 9, 2024, 9:01 PM +02:00

---
+++ test.py
@@ -0,0 +1,20 @@
+import re
+import sys
+
+
+def main():
+    parse(input("HTML: "))
+
+
+def parse(s):
+    if s := re.search(r"<iframe.+?src=\"(https?://)(?:www\.)?(youtube.com)/embed/([^\"]+)\"></iframe>", s, re.IGNORECASE):
+        youtube = s.group(2).replace("youtube.com", "youtu.be/")
+        print(s.group(1), youtube, s.group(3), sep="")
+    else:
+        print(None)
+
+
+
+
+if __name__ == "__main__":
+    main()
