32b71b186850c7671aff994b4995d2f271a415cb
Emilj100 <emilbondo12@gmail.com>
2024-10-09 19:19:09 +0000

    Wed, Oct 9, 2024, 9:19 PM +02:00

--- watch.py
+++ watch.py
@@ -1,20 +1,19 @@
 import re
 import sys
 
-
 def main():
-    parse(input("HTML: "))
-
+    print(parse(input("HTML: ")))
 
 def parse(s):
-    if s := re.search(r"<iframe.+?src=\"(https?://)(?:www\.)?(youtube.com)/embed/([^"]+)\"></iframe>", s, re.IGNORECASE):
-        youtube = s.group(2).replace("youtube.com", "youtu.be/")
-        print(s.group(1), youtube, s.group(3), sep="")
+    # Brug et regulært udtryk til at finde iframe src, der peger på en YouTube-video
+    match = re.search(r'src="https?://(?:www\.)?youtube\.com/embed/([^"]+)"', s, re.IGNORECASE)
+
+    # Hvis vi fandt et match, opbyg den korte URL, ellers returner None
+    if match:
+        video_id = match.group(1)
+        return f"https://youtu.be/{video_id}"
     else:
-        print(None)
-
-
-
+        return None
 
 if __name__ == "__main__":
     main()
