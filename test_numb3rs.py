42f828db48507310f73dbd750bbff0514966f1a2
Emilj100 <emilbondo12@gmail.com>
2024-10-09 14:54:54 +0000

    Wed, Oct 9, 2024, 4:54 PM +02:00

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -2,3 +2,10 @@ from numb3rs import validate
 
 def test_validate():
     assert validate("2.3.4.5") == True
+    assert validate("226.953.245.541") == False
+
+def test_validate_string():
+    assert validate("cat") == False
+    assert validate("test") == False
+
+
