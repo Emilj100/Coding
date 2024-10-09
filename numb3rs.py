fa38b53791a116eaa5ed8b8c83668228862a57ac
Emilj100 <emilbondo12@gmail.com>
2024-10-09 15:09:46 +0000

    Wed, Oct 9, 2024, 5:09 PM +02:00

--- numb3rs.py
+++ numb3rs.py
@@ -12,18 +12,10 @@ def validate(ip):
         ip_4 = int(ip.group(4))
         ip_5 = int(ip.group(5))
         if ip_2 >= 0 and ip_2 <= 255:
-            if ip_3 >= 0 and ip_3 <= 255:
-                if ip_4 >= 0 and ip_4 <= 255:
-                    if ip_5 >= 0 and ip_5 <= 255:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
+            return True
         else:
             return False
+
     else:
         return False
 
