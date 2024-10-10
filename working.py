9a5d97cefebe52efd6a0d67145a4a6e66babf4d5
Emilj100 <emilbondo12@gmail.com>
2024-10-10 12:35:19 +0000

    Thu, Oct 10, 2024, 2:35 PM +02:00

--- working.py
+++ working.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    convert(input("Hours: "))
+    print(convert(input("Hours: ")))
 
 
 def convert(s):
@@ -12,45 +12,53 @@ def convert(s):
 
 
     if match_1:
+
+        number_1_am_match_1 = match_1.group(2)
+        number_2_am_match_1 = match_1.group(3)
+        number_2_pm_match_1 = match_1.group(6)
+
+        if int(match_1.group(2)) == 12:
+            number_1_am_match_1 = match_1.group(5).replace("12", "00")
+
         if int(match_1.group(5)) == 1:
-            number_1 = match_1.group(5).replace("1", "13")
+            number_1_pm_match_1 = match_1.group(5).replace("1", "13")
 
         elif int(match_1.group(5)) == 2:
-            number_1 = match_1.group(5).replace("2", "14")
+            number_1_pm_match_1 = match_1.group(5).replace("2", "14")
 
         elif int(match_1.group(5)) == 3:
-            number_1 = match_1.group(5).replace("3", "15")
+            number_1_pm_match_1 = match_1.group(5).replace("3", "15")
 
         elif int(match_1.group(5)) == 4:
-            number_1 = match_1.group(5).replace("4", "16")
+            number_1_pm_match_1 = match_1.group(5).replace("4", "16")
 
         elif int(match_1.group(5)) == 5:
-            number_1 = match_1.group(5).replace("5", "17")
+            number_1_pm_match_1 = match_1.group(5).replace("5", "17")
 
         elif int(match_1.group(5)) == 6:
-            number_1 = match_1.group(5).replace("6", "18")
+            number_1_pm_match_1 = match_1.group(5).replace("6", "18")
 
         elif int(match_1.group(5)) == 7:
-            number_1 = match_1.group(5).replace("7", "19")
+            number_1_pm_match_1 = match_1.group(5).replace("7", "19")
 
         elif int(match_1.group(5)) == 8:
-            number_1 = match_1.group(5).replace("8", "20")
+            number_1_pm_match_1 = match_1.group(5).replace("8", "20")
 
         elif int(match_1.group(5)) == 9:
-            number_1 = match_1.group(5).replace("9", "21")
+            number_1_pm_match_1 = match_1.group(5).replace("9", "21")
 
         elif int(match_1.group(5)) == 10:
-            number_1 = match_1.group(5).replace("10", "22")
+            number_1_pm_match_1 = match_1.group(5).replace("10", "22")
 
         elif int(match_1.group(5)) == 11:
-            number_1 = match_1.group(5).replace("11", "23")
+            number_1_pm_match_1 = match_1.group(5).replace("11", "23")
 
         elif int(match_1.group(5)) == 12:
-            number_1 = match_1.group(5)
+            number_1_pm_match_1 = match_1.group(5)
 
 
 
-        return f
+        return f"{number_1_am_match_1}:{number_2_am_match_1} to {number_1_pm_match_1}:{number_2_pm_match_1}"
 
 
     elif match_2:
