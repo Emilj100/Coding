0496647b4b3c1de9d528761599a2d60ec1ab6828
Emilj100 <emilbondo12@gmail.com>
2024-10-10 12:05:10 +0000

    Thu, Oct 10, 2024, 2:05 PM +02:00

--- working.py
+++ working.py
@@ -13,7 +13,7 @@ def convert(s):
 
     if match_1:
         if int(match_1.group(5)) == 1:
-            number_1 = match_1.replace(1, 13)
+            number_1 = match_1.group(5).replace(1, 13)
             print(number_1)
     elif match_2:
         print("ja tak")
