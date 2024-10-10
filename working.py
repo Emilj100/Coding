ec721e9a03522906388f8b76f05fd53254a189c6
Emilj100 <emilbondo12@gmail.com>
2024-10-10 12:02:21 +0000

    Thu, Oct 10, 2024, 2:02 PM +02:00

--- working.py
+++ working.py
@@ -7,12 +7,13 @@ def main():
 
 
 def convert(s):
-    match_1 = re.search(r"^(([0-9]+):[0-9]+ AM) to ([0-9]+:[0-9]+ PM)$", s)
+    match_1 = re.search(r"^(([0-9]+):([0-9]+) AM) to (([0-9]+):([0-9]+) PM)$", s)
     match_2 = re.search(r"^(([0-9]+):[0-9]+ PM) to ([0-9]+:[0-9]+ AM)$", s)
 
 
     if match_1:
-        if int(match_1.group(2)) == 
+        if int(match_1.group(5)) == 1:
+            = match_1.replace(1, 13)
     elif match_2:
         print("ja tak")
     else:
