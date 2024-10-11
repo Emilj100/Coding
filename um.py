4ad0dd527ca69c8c24e9e32c1c04182d2da7b04e
Emilj100 <emilbondo12@gmail.com>
2024-10-11 12:51:32 +0000

    Fri, Oct 11, 2024, 2:51 PM +02:00

--- um.py
+++ um.py
@@ -8,9 +8,8 @@ def main():
 
 def count(s):
     match = re.findall(r"\bum\b", s, re.IGNORECASE)
-    counter = list(match)
-    test = len(counter)
-    return test
+    counter = len(list(match))
+    return counter
 
 
 
