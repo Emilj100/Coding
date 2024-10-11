3f54c977fd57c813405749fb54e70f07f2355d2c
Emilj100 <emilbondo12@gmail.com>
2024-10-11 13:20:21 +0000

    Fri, Oct 11, 2024, 3:20 PM +02:00

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
-import re
-import sys
+from um import count
 
 
-def main():
-    print(count(input("Text: ").strip()))
+def test_input():
+    assert count('yummy') == 0
+    assert count('mum ') == 0
 
+def test_nospace():
+    assert count('um') == 1
+    assert count('um, can i... um... help?') == 2
 
-def count(s):
+def test_space():
+    assert count(' um ') == 1
 
-
-    count=re.findall(r'\bum\b', s, re.IGNORECASE)
-
-    return len(count)
-
-
+def test_case():
+    assert count('Um, how you doin') == 1
+    assert count('umm.. UM.. whats um up?') == 2
 if __name__ == "__main__":
     main()
