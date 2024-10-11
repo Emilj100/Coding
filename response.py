c9dfe3834eff27b663902ccb6114da5b3b04465d
Emilj100 <emilbondo12@gmail.com>
2024-10-11 14:33:14 +0000

    Fri, Oct 11, 2024, 4:33 PM +02:00

---
+++ response.py
@@ -0,0 +1,10 @@
+from validator_collection import validators
+
+user_email = input("What's your email address? ")
+
+try:
+    email_address = validators.email(user_email)
+        print("Valid")
+
+except ValueError:
+    print("Invalid")
