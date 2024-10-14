fd7d7f2d74f1345292779ebde974db598d59aa1a
Emilj100 <emilbondo12@gmail.com>
2024-10-14 07:23:50 +0000

    Mon, Oct 14, 2024, 9:23 AM +02:00

--- student.py
+++ student.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
@@ -18,8 +19,9 @@ def main():
 
 def get_student():
     name = input("Name: ")
-    house = input("House: ").title()
-    return Student(name, house)
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 if __name__ == "__main__":
     main()
