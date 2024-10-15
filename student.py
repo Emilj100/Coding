7a8e7e8df5797fd9a16c692f8489758b9d8fa465
Emilj100 <emilbondo12@gmail.com>
2024-10-15 15:14:03 +0000

    Tue, Oct 15, 2024, 5:14 PM +02:00

--- student.py
+++ student.py
@@ -6,31 +6,6 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    # Getter
-    @property
-    def name(self):
-        return self._name
-
-    # Setter
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
-
-    # Getter
-    @property
-    def house(self):
-        return self._house
-
-    # Setter
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
-    
-
 
 def main():
     student = get_student()
