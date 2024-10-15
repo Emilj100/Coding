af0fcee02d9f373131bc1153aec02de73da4c736
Emilj100 <emilbondo12@gmail.com>
2024-10-15 14:41:39 +0000

    Tue, Oct 15, 2024, 4:41 PM +02:00

--- hat.py
+++ hat.py
@@ -1,8 +1,8 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
     def sort(self, name):
         print(name, "is in", random.choice(self.houses))
