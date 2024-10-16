a1f19e76ba4238799d855ba338f0b0356c3d2e79
Emilj100 <emilbondo12@gmail.com>
2024-10-16 17:14:28 +0000

    Wed, Oct 16, 2024, 7:14 PM +02:00

--- seasons.py
+++ seasons.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 class Date:
     def __init__(self, year, month, day):
@@ -20,13 +20,15 @@ class Date:
 
 def main():
     user_date_year, user_date_month, user_date_day = input("Date of Birth: ").split("-")
-    user_date = Date(int(user_date_year), int(user_date_month), int(user_date_day))
+    user_date = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
 
-    date_now = date.today()
+    date_now = datetime.today()
+    print(date_now)
 
     date_now_year, date_now_month, date_now_day = str(date_now).split("-")
+    date_now_day, remove_time = str(date_now).split(" ")
     date_now = Date(int(date_now_year), int(date_now_month), int(date_now_day))
-    
+
     minutes = date_now - user_date
     print(minutes)
 
