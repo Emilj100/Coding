b720eb3a51e34805735e6ad78febf957b1953d21
Emilj100 <emilbondo12@gmail.com>
2024-10-16 12:43:31 +0000

    Wed, Oct 16, 2024, 2:43 PM +02:00

--- seasons.py
+++ seasons.py
@@ -6,14 +6,16 @@ class Date:
         self.month = month
         self.day = day
 
-    def __sub__(self, other):
+
 
 
 
 def main():
     user_date_year, user_date_month, user_date_day = input("Date of Birth: ").split("-")
-    user_date = date(user_date_year, user_date_month, user_date_day)
-    date_now = date.today().split("-")
+    user_date = Date(user_date_year, user_date_month, user_date_day)
+    date_now = date.today()
+    date_now_year, date_now_month, date_now_day = date_now.split("-")
+    print(date_now_year)
 
 
 
