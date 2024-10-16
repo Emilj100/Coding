0906570c39224cd15dfffccf001cc8ecb45c29cf
Emilj100 <emilbondo12@gmail.com>
2024-10-16 17:47:32 +0000

    Wed, Oct 16, 2024, 7:47 PM +02:00

--- seasons.py
+++ seasons.py
@@ -35,7 +35,7 @@ def main():
     minutes = int(days) * 24 * 60
 
     minutes = inflect.engine().number_to_words(int(minutes), andword="")
-    print(minutes)
+    print f"{minutes.title()} minutes"
 
 
 
