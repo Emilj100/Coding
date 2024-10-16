6ae91b350e0d8e8f848e0b3ae532265079e61caf
Emilj100 <emilbondo12@gmail.com>
2024-10-16 17:06:45 +0000

    Wed, Oct 16, 2024, 7:06 PM +02:00

--- seasons.py
+++ seasons.py
@@ -21,9 +21,12 @@ class Date:
 def main():
     user_date_year, user_date_month, user_date_day = input("Date of Birth: ").split("-")
     user_date = Date(int(user_date_year), int(user_date_month), int(user_date_day))
+
     date_now = date.today()
+
     date_now_year, date_now_month, date_now_day = str(date_now).split("-")
     date_now = Date(int(date_now_year), int(date_now_month), int(date_now_day))
+    
     minutes = date_now - user_date
     print(minutes)
 
