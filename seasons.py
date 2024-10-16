885ed11d9d680255f6f304b6bdfef2d0a9d4d78c
Emilj100 <emilbondo12@gmail.com>
2024-10-16 08:32:08 +0000

    Wed, Oct 16, 2024, 10:32 AM +02:00

--- seasons.py
+++ seasons.py
@@ -1,8 +1,22 @@
 from datetime import date
 
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+
+
+
+    def __sub__(self, other):
+
+
 
 def main():
-    user_date = input("Date of Birth: ")
+    user_date_year, user_date_month, user_date_day = input("Date of Birth: ").split("-")
+    user_date = Date(user_date_year, user_date_month, user_date_day)
+
 
 
 
