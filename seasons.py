55ba664e169a25175b4c65acd51f82f0d038b112
Emilj100 <emilbondo12@gmail.com>
2024-10-17 08:34:32 +0000

    Thu, Oct 17, 2024, 10:34 AM +02:00

--- seasons.py
+++ seasons.py
@@ -6,7 +6,11 @@ months = ["january", "february", "march", "april", "may", "june", "july", "augus
 
 def main():
     date1 = today()
-    date2 = user_input()
+    try:
+        user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
+    except ValueError:
+        sys.exit("Wrong input")
+    date2 = user_input(user_date_year, user_date_month, user_date_day)
 
 
     days = date1 - date2
@@ -24,9 +28,7 @@ def today():
     return date1
 
 
-def user_input():
-    try:
-        user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
+def user_input(user_date_year, user_date_month, user_date_day):
         date2 = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
         return date2
 
