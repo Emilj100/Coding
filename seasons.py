fcd50afa4e22310680081aa20a7c7a4fc60f9359
Emilj100 <emilbondo12@gmail.com>
2024-10-17 07:50:38 +0000

    Thu, Oct 17, 2024, 9:50 AM +02:00

--- seasons.py
+++ seasons.py
@@ -10,10 +10,12 @@ def main():
     date_now_year, date_now_month, date_now_day = str(date_now).split("-")
     date1 = datetime(int(date_now_year), int(date_now_month), int(date_now_day))
 
-    user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
-    if user_date_year in months:
+    try:
+        user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
+        date2 = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
+
+    except ValueError:
         sys.exit("Wrong input")
-    date2 = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
 
 
 
