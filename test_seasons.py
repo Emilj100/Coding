363c051e0e3c4c7aac2963c5dc259eb085ddfbbc
Emilj100 <emilbondo12@gmail.com>
2024-10-17 08:49:17 +0000

    Thu, Oct 17, 2024, 10:49 AM +02:00

--- test_seasons.py
+++ test_seasons.py
@@ -4,7 +4,3 @@ import pytest
 def test_user_input_wrong():
     with pytest.raises(SystemExit):
         user_input("February 6t, 1998")
-
-def test_user_input_corret():
-    assert user_input("2005-01-10") == "Ten million, three hundred ninety-six thousand, eight hundred minutes"
-
