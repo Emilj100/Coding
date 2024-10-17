3d6c9efa7ebd3e2ac4e1ceea7984e49f37f8f908
Emilj100 <emilbondo12@gmail.com>
2024-10-17 08:45:55 +0000

    Thu, Oct 17, 2024, 10:45 AM +02:00

--- test_seasons.py
+++ test_seasons.py
@@ -1,6 +1,9 @@
 from seasons import user_input
 import pytest
 
-def test_user_input():
-    with pytest.raises(ValueError):
+def test_user_input_wrong():
+    with pytest.raises(SystemExit):
         user_input("February 6t, 1998")
+
+def test_user_input_corret():
+    assert user_input("2005-01-10") ==
