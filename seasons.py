6ec76ad8fe7844ff9e9ddae10b93e4abe1135149
Emilj100 <emilbondo12@gmail.com>
2024-10-17 07:43:50 +0000

    Thu, Oct 17, 2024, 9:43 AM +02:00

--- seasons.py
+++ seasons.py
@@ -1,24 +1,6 @@
 from datetime import date, datetime
 import inflect
 
-class Date:
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-
-    def __str__(self):
-        return f"{self.year}-{self.month}-{self.day}"
-
-
-    def __sub__(self, other):
-        year = self.year - other.year
-        month = self.month - other.month
-        day = self.day - other.day
-        return Date(year, month, day)
-
-
-
 def main():
     date_now = date.today()
 
