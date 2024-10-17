04f979ae92540dd834b40021cbe222034d774cc9
Emilj100 <emilbondo12@gmail.com>
2024-10-17 07:54:44 +0000

    Thu, Oct 17, 2024, 9:54 AM +02:00

--- seasons.py
+++ seasons.py
@@ -5,18 +5,8 @@ import sys
 months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "oktober", "november", "december"]
 
 def main():
-    date_now = date.today()
-
-    date_now_year, date_now_month, date_now_day = str(date_now).split("-")
-    date1 = datetime(int(date_now_year), int(date_now_month), int(date_now_day))
-
-    try:
-        user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
-        date2 = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
-
-    except ValueError:
-        sys.exit("Wrong input")
-
+    date1 = today()
+    date2 = user_input()
 
 
     days = date1 - date2
@@ -26,6 +16,22 @@ def main():
     minutes = inflect.engine().number_to_words(int(minutes), andword="")
     print(f"{minutes.capitalize()} minutes")
 
+def today():
+    date_now = date.today()
+
+    date_now_year, date_now_month, date_now_day = str(date_now).split("-")
+    date1 = datetime(int(date_now_year), int(date_now_month), int(date_now_day))
+    return date1
+
+
+def user_input():
+    try:
+        user_date_year, user_date_month, user_date_day = input("Date of Birth: ").lower().split("-")
+        date2 = datetime(int(user_date_year), int(user_date_month), int(user_date_day))
+        return date2
+
+    except ValueError:
+        sys.exit("Wrong input")
 
 
 if __name__ == "__main__":
