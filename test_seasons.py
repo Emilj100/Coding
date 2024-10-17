8d12e858e7758538f6496d74e338ad4c96b590d3
Emilj100 <emilbondo12@gmail.com>
2024-10-17 08:47:22 +0000

    Thu, Oct 17, 2024, 10:47 AM +02:00

--- test_seasons.py
+++ test_seasons.py
@@ -6,4 +6,5 @@ def test_user_input_wrong():
         user_input("February 6t, 1998")
 
 def test_user_input_corret():
-    assert user_input("2005-01-10") ==
+    assert user_input("2005-01-10") == "Ten million, three hundred ninety-six thousand, eight hundred minutes"
+
