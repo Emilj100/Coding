ad11d5f96b0a04885899597212820111a7fa8208
Emilj100 <emilbondo12@gmail.com>
2024-10-17 16:37:39 +0000

    Thu, Oct 17, 2024, 6:37 PM +02:00

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ cookies = []
 def main():
     amount = input("How many cookies in jar? ")
     test(int(amount))
-    print(cookies)
+    for cookie in cookies:
+        print(cookie, end="")
+    print()
 
 def test(amount):
     for _ in range(amount):
