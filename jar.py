a800a7c81dbb19b2102a630f42fa014a7ff68957
Emilj100 <emilbondo12@gmail.com>
2024-10-18 19:01:26 +0000

    Fri, Oct 18, 2024, 9:01 PM +02:00

--- jar.py
+++ jar.py
@@ -2,17 +2,17 @@ class Jar:
     def __init__(self, capacity=12):
         if capacity < 0:
             raise ValueError("Capacity must be a non-negative integer.")
-        self._capacity = capacity  # Privat variabel til kapacitet
-        self._size = 0  # Start med 0 cookies i krukken
+        self._capacity = capacity
+        self._size = 0
 
     def __str__(self):
-        return "🍪" * self._size  # Returnér det korrekte antal cookies som en streng
+        return "🍪" * self._size
 
     def deposit(self, n):
-        # Tjek om der er plads til flere cookies uden at overskride kapaciteten
+
         if self._size + n > self._capacity:
             raise ValueError("Too many cookies!")
-        self._size += n  # Tilføj cookies til krukken
+        self._size += n
 
     def withdraw(self, n):
         if self._size - n < 0:
@@ -22,27 +22,27 @@ class Jar:
 
     @property
     def size(self):
-        return self._size  # Returnér det aktuelle antal cookies i krukken
+        return self._size
 
     @property
     def capacity(self):
-        return self._capacity  # Returnér kapaciteten af krukken
+        return self._capacity
 
 
 def main():
     # Til capacity
     amount = input("What is the max capacity in the cookie jar? ")
-    jar = Jar(int(amount))  # Opret en krukke med den ønskede kapacitet
+    jar = Jar(int(amount))
 
     # Til deposit
     user_deposit = int(input("How many cookies would you like to deposit? "))
-    jar.deposit(user_deposit)  # Tilføj cookies til krukken
+    jar.deposit(user_deposit)
 
     # Til withdraw
     user_withdraw = int(input("How many cookies would you like to withdraw? "))
     jar.withdraw(user_withdraw)
 
-    # Print den opdaterede krukke med cookies
+
     print(jar)
 
 
--- test_seasons.py
+++
@@ -1,6 +0,0 @@
-from seasons import user_input
-import pytest
-
-def test_user_input_wrong():
-    with pytest.raises(SystemExit):
-        user_input("February 6t, 1998")
