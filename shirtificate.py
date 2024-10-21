e2a89f9ecbdd5f93bd6d5e7154e3fd42960c3798
Emilj100 <emilbondo12@gmail.com>
2024-10-21 08:59:56 +0000

    Mon, Oct 21, 2024, 10:59 AM +02:00

--- shirtificate.py
+++ shirtificate.py
@@ -1,1 +1,8 @@
-user_name = input("Name: ")
+from fpdf import FPDF
+
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("helvetica", "B", 16)
+pdf.cell(80, 10, "CS50 Shirtificate")
+pdf.output("tuto1.pdf")
