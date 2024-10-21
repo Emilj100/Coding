8db7cf5c6b69b2c361cdd6e357f7cead66117023
Emilj100 <emilbondo12@gmail.com>
2024-10-21 09:29:04 +0000

    Mon, Oct 21, 2024, 11:29 AM +02:00

--- shirtificate.py
+++ shirtificate.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 
+user_name = input("Name: ")
 
 pdf = FPDF()
 pdf.add_page()
@@ -7,7 +8,7 @@ pdf.set_font("helvetica", "B", 16)
 pdf.cell(70)
 pdf.cell(40, 20, "CS50 Shirtificate")
 pdf.image("image.png", x=50, y=40, w=100)
-pdf.set_font("helvetica", "B", 16)
+pdf.set_text_color(255, 255, 255)
 pdf.cell(-40)
-pdf.cell(20, 130, "test")
+pdf.cell(20, 130, user_name)
 pdf.output("shirtificate.pdf")
