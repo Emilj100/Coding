dbc04f7ada50a0343fc8b23355a7d037232d04f1
Emilj100 <emilbondo12@gmail.com>
2024-10-21 09:03:20 +0000

    Mon, Oct 21, 2024, 11:03 AM +02:00

--- shirtificate.py
+++ shirtificate.py
@@ -4,5 +4,5 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
-pdf.cell(80, 10, "CS50 Shirtificate")
+pdf.cell(40, 20, "CS50 Shirtificate")
 pdf.output("tuto1.pdf")
