7a3415e28d292923e22cfd7808467f2997ea58be
Emilj100 <emilbondo12@gmail.com>
2024-10-22 08:33:52 +0000

    Tue, Oct 22, 2024, 10:33 AM +02:00

--- houses.py
+++ houses.py
@@ -6,9 +6,10 @@ students = [
     {"name": "Padma", "house": "Ravenclaw"},
 ]
 
-houses = set()
+houses = []
 for student in students:
     if student["house"] not in houses:
+        houses.append(student["house"])
 
 for house in sorted(houses):
     print(house)
