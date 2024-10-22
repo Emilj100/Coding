a5a284b799240778be0252312920c244a60170e6
Emilj100 <emilbondo12@gmail.com>
2024-10-22 07:45:08 +0000

    Tue, Oct 22, 2024, 9:45 AM +02:00

--- houses.py
+++ houses.py
@@ -8,8 +8,7 @@ students = [
 
 houses = set()
 for student in students:
-    if student["house"] not in houses:
-        houses.append(student["house"])
+    houses.add(student["house"])
 
 for house in sorted(houses):
     print(house)
