f9a1cb565e7bb772a68cd70185f638c2bcac37ef
Emilj100 <emilbondo12@gmail.com>
2024-10-22 07:58:23 +0000

    Tue, Oct 22, 2024, 9:58 AM +02:00

--- houses.py
+++ houses.py
@@ -2,7 +2,7 @@ students = [
     {"name": "Hermione", "house": "Gryffindor"},
     {"name": "Harry", "house": "Gryffindor"},
     {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco" "house": "Slytherin"},
+    {"name": "Draco", "house": "Slytherin"},
     {"name": "Padma", "house": "Ravenclaw"},
 ]
 
