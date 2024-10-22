a8fd16da64f92a45d999c028174fc4b660274318
Emilj100 <emilbondo12@gmail.com>
2024-10-22 07:15:38 +0000

    Tue, Oct 22, 2024, 9:15 AM +02:00

--- houses.py
+++ houses.py
@@ -1,5 +1,12 @@
 students = [
     {"name": "Hermione", "house": "Gryffindor"},
     {"name": "Harry", "house" "Gryffindor"},
-    {"name": "Ron", "}
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco"m "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
 ]
+
+houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append()
