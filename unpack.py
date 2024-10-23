5af906c16df5b52d22507c40156da543ef2870c1
Emilj100 <emilbondo12@gmail.com>
2024-10-23 09:08:25 +0000

    Wed, Oct 23, 2024, 11:08 AM +02:00

--- unpack.py
+++ unpack.py
@@ -1,2 +1,4 @@
-first, last = input("What's your name? ").split(" ")
-print(f"hello, {first}")
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+total(100, 50, 25)
