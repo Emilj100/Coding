705feef18061b43d2026dfcd748248cd74283a0e
Emilj100 <emilbondo12@gmail.com>
2024-10-23 12:52:03 +0000

    Wed, Oct 23, 2024, 2:52 PM +02:00

--- gryffindors.py
+++ gryffindors.py
@@ -1,8 +1,6 @@
 students = ["Hermione", "Harry", "Ron"]
 
-gryffindors = []
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
 
-for student in students:
-    gryffindors.append({"name": student, "house": "Gryffindor"})
 
 print(gryffindors)
