b3f211388274eb666990c73f77018b6104139fe9
Emilj100 <emilbondo12@gmail.com>
2024-10-24 15:09:15 +0000

    Thu, Oct 24, 2024, 5:09 PM +02:00

--- foodapi.py
+++ foodapi.py
@@ -31,7 +31,6 @@ nutrition_data = response.json()
 
 # Vis det fulde JSON-svar (kan bruges til at analysere data)
 
-for food in nutrition_data["foods"]:
-    print(food["nf_calories"])
 
 
+print(json.dumps(nutrition_data, indent=4))
---
+++ test.py
@@ -0,0 +1,4 @@
+print("Here is the data for the food you have been eating today")
+
+for food in nutrition_data["foods"]:
+    print(food["nf_calories"])
