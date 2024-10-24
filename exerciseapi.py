7198d438fec92503bf5053ec2014fba77f593498
Emilj100 <emilbondo12@gmail.com>
2024-10-24 16:44:39 +0000

    Thu, Oct 24, 2024, 6:44 PM +02:00

--- exerciseapi.py
+++ exerciseapi.py
@@ -2,13 +2,11 @@ import requests
 
 url = "https://exercisedb.p.rapidapi.com/exercises"
 
-querystring = {"limit":"10","offset":"0"}
-
 headers = {
 	"x-rapidapi-key": "6992181b32msh2971ae1097ca54dp1a1276jsn8ecf6bcb9516",
 	"x-rapidapi-host": "exercisedb.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+response = requests.get(url, headers=headers)
 
 print(response.json())
