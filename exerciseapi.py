aea1f5b23f717662228eb81c0ef96954a3008f25
Emilj100 <emilbondo12@gmail.com>
2024-10-24 16:49:22 +0000

    Thu, Oct 24, 2024, 6:49 PM +02:00

--- exerciseapi.py
+++ exerciseapi.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://exercisedb.p.rapidapi.com/exercises"
 
-querystring = {"limit":"0","offset":"0"}
+querystring = {"limit":"20","offset":"0"}
 
 headers = {
 	"x-rapidapi-key": "6992181b32msh2971ae1097ca54dp1a1276jsn8ecf6bcb9516",
