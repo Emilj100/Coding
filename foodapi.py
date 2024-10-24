984d526a82933fb18e6b33a36d4b1b85a2ae23b6
Emilj100 <emilbondo12@gmail.com>
2024-10-24 15:30:36 +0000

    Thu, Oct 24, 2024, 5:30 PM +02:00

--- foodapi.py
+++ foodapi.py
@@ -34,10 +34,10 @@ nutrition_data = response.json()
 print("Here is the data for the food you have been eating today")
 
 for food in nutrition_data["foods"]:
-    print(f'{food["nf_calories"]} calories')
-    print(f'{food["nf_protein"]} protein')
-    print(f'{food["nf_total_carbohydrate"]} carbohydrate')
-    print(f'{food["nf_total_fat"]} fat')
+    calories = food["nf_calories"]
+    total = sum(calories)
+    print(total, "calories")
+
 
 
 
