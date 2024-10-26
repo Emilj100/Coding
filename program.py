34d42c217a67da046267ec753f4d1086af90cd41
Emilj100 <emilbondo12@gmail.com>
2024-10-26 15:31:26 +0000

    Sat, Oct 26, 2024, 5:31 PM +02:00

--- program.py
+++ program.py
@@ -8,28 +8,35 @@ user_options = ["1", "2", "3", "4", "5"]
 
 # Programmet skal tjekke om brugerens navn allerede er i en csv fil med brugerens oplysninger (Brug evt. While True loop eller lign for at sikre at brugeren indtaster 1,2,3,4 eller 5. Tjek gerne dine noter for at finde ud af hvordan man).
 if user_name in ...:
+
+
     while True:
         user_input = input("What would you like to do\n 1. track calories\n 2. See my trainingprogram and calorie intake\n 3. Update my data\n 4. Change my trainingprogram\n 5. Exit\n (Enter: 1,2,3,4 or 5)")
-        if not user_input in user_options:
-            break
-        elif user_input == "1":
-            # Start track calories program
+
+        if user_input == "1":
+                # Start track calories program
+                print("1")
 
         elif user_input == "2":
-            # Vis brugerens træningsprogrammer og calorie intake
+                # Vis brugerens træningsprogrammer og calorie intake
+                print("2")
 
         elif user_input == "3":
-            # Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+                # Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+                print("3")
 
         elif user_input == "4":
-            # Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+                # Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+                print("4")
 
         elif user_input == "5":
             # Exit programmet
-            sys.exit()
-        else:
-            # Find ud af hvordan du beder brugeren om at indtaste igen indtil han indtaster et valid input
+            sys.exit("5")
 
+    if user_input in user_options:
+          break
+
+    # Find ud af hvordan du beder brugeren om at indtaste igen indtil han indtaster et valid input
 else:
     print("Welcome! First we need some data to get the right program for you")
     name = input("Name: ")
