a8eb93bac4558d32e98ceba2862bae8d3e620e9d
Emilj100 <emilbondo12@gmail.com>
2024-10-26 10:51:49 +0000

    Sat, Oct 26, 2024, 12:51 PM +02:00

---
+++ program.py
@@ -0,0 +1,73 @@
+Programmet spørger om brugerens navn:
+
+if statement: Hvis navnet er i systemet spørger programmet brugeren om følgende:
+
+What would you like to do 1. track calories 2. See my trainingprogram and calorie intake 3. Update my data 4. Change my trainingprogram 5. Exit (Enter: 1,2,3,4 or 5)
+
+if user_input == 1:
+
+Start track calories program
+
+if user_input == 2:
+
+Vis brugerens træningsprogrammer og calorie intake
+
+if user_input == 3:
+
+Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+
+if user_input == 4:
+
+Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+
+if user_input == 5:
+
+sys.exit()
+
+if statement: Hvis navnet ikke er i systemet skal systemet bede om følgende oplysnger
+
+print(”Welcome! First we need some data to get the right program for you
+
+name = input(”Name: ”)
+
+gender = input(”Male/Female: “)
+
+height = input(”height: “)
+
+age = input(”age: “)
+
+weight = input(”weight: “)
+
+print(”Nice! Let us know a bit more about your goals and how many days you want to train per week.
+
+goal = input(”What is your goal? 1. To lose weight 2. Stay at my current weight 3. Gain weight (Enter 1,2 or 3))
+
+training_days = input(”How many days would you like to train per week?
+
+print(”Great! Thats all we needed. Here is your new program and calorie intake”)
+
+Vis brugerens træningsprogram og calorie intake udfra de oplysninger de har skrevet.
+
+(Herefter spørger vi brugeren om det samme som i det allerførste if statement, så det kører i loop indtil brugeren forlader programmet)
+
+input(”What would you like to do 1. track calories 2. See my trainingprogram and calorie intake 3. Update my data 4. Change my trainingprogram 5. Exit (Enter: 1,2,3,4 or 5)”)
+
+if user_input == 1:
+
+Start track calories program
+
+if user_input == 2:
+
+Vis brugerens træningsprogrammer og calorie intake
+
+if user_input == 3:
+
+Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+
+if user_input == 4:
+
+Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+
+if user_input == 5:
+
+sys.exit()
