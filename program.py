957127f5a4f1285b4d04896003f7f08e1b4050fb
Emilj100 <emilbondo12@gmail.com>
2024-10-27 09:42:25 +0000

    Sun, Oct 27, 2024, 10:42 AM +01:00

--- program.py
+++ program.py
@@ -53,22 +53,28 @@ else:
 
 # Herefter spørger vi brugeren om det samme som i det allerførste if statement, så det kører i loop indtil brugeren forlader programmet
 
-    user_input = input("What would you like to do\n 1. track calories\n 2. See my trainingprogram and calorie intake\n 3. Update my data\n 4. Change my trainingprogram\n 5. Exit\n (Enter: 1,2,3,4 or 5)")
-    if user_input in user_options:
+    while True:
+        user_input = input("What would you like to do\n 1. track calories\n 2. See my trainingprogram and calorie intake\n 3. Update my data\n 4. Change my trainingprogram\n 5. Exit\n (Enter: 1,2,3,4 or 5)")
+
         if user_input == "1":
-            # Start track calories program
+                # Start track calories program
+                print("1")
 
         elif user_input == "2":
-            # Vis brugerens træningsprogrammer og calorie intake
+                # Vis brugerens træningsprogrammer og calorie intake
+                print("2")
 
         elif user_input == "3":
-            # Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+                # Giv brugeren mulighed for at opdatere sin data, som navn, vægt osv
+                print("3")
 
         elif user_input == "4":
-            # Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+                # Bed brugeren om at indtaste hvor mange gange de ønsker at træne om ugen igen.
+                print("4")
 
         elif user_input == "5":
             # Exit programmet
-            sys.exit()
-    else:
-        # Find ud af hvordan du beder brugeren om at indtaste igen indtil han indtaster et valid input
+            sys.exit("5")
+
+        if user_input in user_options:
+            break
