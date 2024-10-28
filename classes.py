27f3cbb424288e0b11df414c4c4f90713179c496
Emilj100 <emilbondo12@gmail.com>
2024-10-28 09:19:50 +0000

    Mon, Oct 28, 2024, 10:19 AM +01:00

--- classes.py
+++ classes.py
@@ -46,7 +46,7 @@ class User:
     @height.setter
     def height(self, height):
         while True:
-            if height := re.fullmatch(r"[1-9])
+            if height := re.fullmatch(r"[0-9]{3})
 
 class Training:
 
