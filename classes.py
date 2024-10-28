7a574e48f0f44f0ee0c7cfd2e89f2533657a855c
Emilj100 <emilbondo12@gmail.com>
2024-10-28 10:24:24 +0000

    Mon, Oct 28, 2024, 11:24 AM +01:00

--- classes.py
+++ classes.py
@@ -76,7 +76,7 @@ class User:
     @weight.setter
     def weight(self, weight):
         while True:
-            if age := re.fullmatch(r" [0-9,]{2,4})
+            if weight := re.fullmatch(r" [0-9,]{2,4}( )?(kg)?)
 
 
 class Training:
