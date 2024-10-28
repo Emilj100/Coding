3edef8ea1d614d9ceea02a319e430b1b75225235
Emilj100 <emilbondo12@gmail.com>
2024-10-28 09:25:37 +0000

    Mon, Oct 28, 2024, 10:25 AM +01:00

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import re
 
-name = input("Name: ")
+height = input("Height: ")
 
-if name := re.fullmatch(r"[a-zA-Z]", name):
-        print("test")
+if height := re.fullmatch(r"[0-9]{3}( )?(cm)?"):
+        print("valid")
 
-print("hej")
+else:
+print("invalid")
