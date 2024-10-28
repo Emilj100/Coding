50e5333235cce94c627660874ac1bc43ffb71ea2
Emilj100 <emilbondo12@gmail.com>
2024-10-28 09:28:19 +0000

    Mon, Oct 28, 2024, 10:28 AM +01:00

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import re
 
 height = input("Height: ")
 
-if height := re.fullmatch(r"[0-9]{3}( )?(cm)?"):
+if height := re.fullmatch(r"[0-9]{3}( )?(cm)?", height, re.IGNORECASE):
         print("valid")
 
 else:
