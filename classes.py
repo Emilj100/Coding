ee0b7bbe8c356f24c09f997655d72fff157e59e4
Emilj100 <emilbondo12@gmail.com>
2024-10-28 07:51:27 +0000

    Mon, Oct 28, 2024, 8:51 AM +01:00

--- classes.py
+++ classes.py
@@ -1,6 +1,14 @@
 
 
 class User:
+    def __init__(self, name, gender, height, age, weight, goal, training)
+        self.name = name
+        self.gender = gender
+        self.height = height
+        self.age = age
+        self.weight = weight
+        self.goal = goal
+        self.training = training
 
 
 
