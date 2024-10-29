5c248b9630c90d5059b9be90274dde884b14190f
Emilj100 <emilbondo12@gmail.com>
2024-10-29 12:07:20 +0000

    Tue, Oct 29, 2024, 1:07 PM +01:00

---
+++ fullprogram.py
@@ -0,0 +1,9 @@
+import sys
+import csv
+
+# Vi skal have en main funktion og derfra flere def funktioner som vi kalder på samt classer
+
+def main():
+    user_name = input("What's your name? ")
+    
+
