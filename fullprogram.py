9d9fe26742d4e9268fdc5069cba88bcd84fedfc2
Emilj100 <emilbondo12@gmail.com>
2024-10-29 18:29:12 +0000

    Tue, Oct 29, 2024, 7:29 PM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -27,7 +27,7 @@ class User:
         with open("data.csv") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                users_name = row["user_name"]
+                users_name = row["name"]
                 users[users_name] = ({"name": row["name"], "gender": row["gender"], "height": row["height"], "age": row["age"], "weight": row["weight"], "goal": row["goal"], "training": row["training"]})
 
     @staticmethod
