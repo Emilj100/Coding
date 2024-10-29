e4d41880611ae01b30c5571e5882420e907dd0da
Emilj100 <emilbondo12@gmail.com>
2024-10-29 20:16:55 +0000

    Tue, Oct 29, 2024, 9:16 PM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -170,16 +170,17 @@ def main():
     else:
         # Få data på brugeren og gem det i en CSV fil
         print(f"Welcome {user_name}! First we need some data to get the right program for you.")
-        name = user_name
-        gender = input("Male/Female: ")
-        height = input("Height: ")
-        age = input("Age: ")
-        weight = input("Weight: ")
+        user = User()
+        user.name = user_name
+        user.gender = input("Male/Female: ")
+        user.height = input("Height: ")
+        user.age = input("Age: ")
+        user.weight = input("Weight: ")
 
         print(f"\nNice {name}! Let us know a bit more about your goals and how many days you want to train per week.\n")
-        goal = input("What is your goal?\n 1. To lose weight\n 2. Stay at my current weight\n 3. Gain weight\n (Enter 1,2 or 3)\n")
-        training = input("How many days would you like to train per week?\n (Enter 1,2,3,4,5,6 or 7)\n")
+        user.goal = input("What is your goal?\n 1. To lose weight\n 2. Stay at my current weight\n 3. Gain weight\n (Enter 1,2 or 3)\n")
+        user.training = input("How many days would you like to train per week?\n (Enter 1,2,3,4,5,6 or 7)\n")
 
-        users[user_name] = User(name, gender, height, age, weight, goal, training)
+        users[user_name] = User(user.name, user.gender, user.height, user.age, user.weight, user.goal, user.training)
 
 main()
