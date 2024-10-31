9a51d9b13c5360cc5e8ee48bd3467e980a0acb80
Emilj100 <emilbondo12@gmail.com>
2024-10-31 09:34:36 +0000

    Thu, Oct 31, 2024, 10:34 AM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -44,6 +44,8 @@ class User:
     def calorie_intake(self, gender, height, age, weight, goal, training):
         if gender == "male":
             bmr = 10 * int(weight) + 6.25 * int(height) - 5 * int(age) + 5
+            if training == "1" or training == "2" or training == "3":
+                training = 1.375
             calorie_intake = bmr * int(training)
             if goal == "1":
                 calorie_intake = calorie_intake - 500
