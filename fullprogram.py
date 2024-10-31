92ef29f4f5d3ba1869ce60e3310fdc39fa193144
Emilj100 <emilbondo12@gmail.com>
2024-10-31 11:28:24 +0000

    Thu, Oct 31, 2024, 12:28 PM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -104,7 +104,6 @@ def main():
     # Tjekker om det indtastede navn allerede eksistere i systemet
     if User.check_user(user_name):
         #################### Find ud af hvordan du gør brug af brugerens objekt hvis han eksistere i programmet i forvejen
-        print(users[user_name])
         user_program_options(user_name)
 
 
