48409d654fc294c4c6116c52d6fb4abe94d45462
Emilj100 <emilbondo12@gmail.com>
2024-10-31 11:29:01 +0000

    Thu, Oct 31, 2024, 12:29 PM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -195,6 +195,8 @@ def user_training_program(training):
 def user_program_options(user_name):
     while True:
 
+        current_user = users[user_name]
+
         user_input = input("What would you like to do\n 1. track calories\n 2. See my trainingprogram and calorie intake\n 3. Update my data\n 4. Change my trainingprogram\n 5. Exit\n (Enter: 1,2,3,4 or 5)")
 
         if user_input == "1":
