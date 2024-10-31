c0f754d4d63e4d195c08013363be978dceccbcbb
Emilj100 <emilbondo12@gmail.com>
2024-10-31 09:41:24 +0000

    Thu, Oct 31, 2024, 10:41 AM +01:00

--- fullprogram.py
+++ fullprogram.py
@@ -44,20 +44,22 @@ class User:
     def calorie_intake(self, gender, height, age, weight, goal, training):
         if gender == "male":
             bmr = 10 * int(weight) + 6.25 * int(height) - 5 * int(age) + 5
-            if training == "1" or training == "2" or training == "3":
-                training = 1.375
-            calorie_intake = bmr * int(training)
-            if goal == "1":
-                calorie_intake = calorie_intake - 500
-            elif goal == "3":
-                calorie_intake = calorie_intake + 500
         if gender == "female":
             bmr = 10 * int(weight) + 6.25 * int(height) - 5 * int(age) - 161
-            calorie_intake = bmr * int(training)
-            if goal == "1":
-                calorie_intake = calorie_intake - 500
-            elif goal == "3":
-                calorie_intake = calorie_intake + 500
+
+        if training == "1" or training == "2" or training == "3":
+            training = 1.375
+        elif training == "4" or training == "5":
+            training == 1.55
+        elif training == "6" or training == "7":
+            training == 1.725
+
+        calorie_intake = bmr * training
+
+        if goal == "1":
+            calorie_intake = calorie_intake - 500
+        elif goal == "3":
+            calorie_intake = calorie_intake + 500
         print(f"This is your calorie intake {calorie_intake}")
 
 
