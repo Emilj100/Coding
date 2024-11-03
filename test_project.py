01e908c70bbeca180e7c311a8078a8917cb8c0f9
Emilj100 <emilbondo12@gmail.com>
2024-11-03 15:53:55 +0000

    Sun, Nov 3, 2024, 4:53 PM +01:00

--- test_project.py
+++ test_project.py
@@ -3,4 +3,4 @@ import pytest
 
 def test_calorie_intake():
     user = User("Emil", "Male", "186", "19", "100.5", "1", "5")
-    user.calorie_intake()
+    user.calorie_intake(weight=100.5, height=186, age=19)
