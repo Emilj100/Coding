22cda9b531b00ecd7d7eed166d84cd3e6950d4e2
Emilj100 <emilbondo12@gmail.com>
2024-11-03 16:48:36 +0000

    Sun, Nov 3, 2024, 5:48 PM +01:00

--- test_project.py
+++ test_project.py
@@ -4,6 +4,6 @@ import pytest
 def test_calorie_intake(capsys):
     user = User("Emil", "Male", "186", "19", "100.5", "1", "5")
     user.calorie_intake()
-    capsys_output = capsys.readouterr()
+    capsys_output.out = capsys.readouterr()
     expected_output = "\nThis is your calorie intake: 2719.35 calories\n"
-    assert capsys_output == expected_output
+    assert capsys_output.out == expected_output
