f1fa27afd7fc4dc2874d097d4c6ffa309ba726b8
Emilj100 <emilbondo12@gmail.com>
2024-11-03 15:59:24 +0000

    Sun, Nov 3, 2024, 4:59 PM +01:00

--- test_project.py
+++ test_project.py
@@ -4,4 +4,4 @@ import pytest
 def test_calorie_intake():
     user = User("Emil", "Male", 186, 19, 100.5, 1, 5)
     user.calorie_intake()
-    assert calorie_intake == 
+    assert calorie_intake == 2719.35
