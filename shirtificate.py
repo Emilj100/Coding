c89ba16f9d49e10fd7dda7fd2770fc23b002aece
Emilj100 <emilbondo12@gmail.com>
2024-11-04 13:47:52 +0000

    Mon, Nov 4, 2024, 2:47 PM +01:00

--- shirtificate.py
+++ shirtificate.py
@@ -8,12 +8,26 @@ pdf.add_page()
 pdf.set_font("helvetica", "B", 24)
 pdf.cell(0, 10, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align="C")
 
-pdf.image("shirtificate.png", x=pdf.epw / 2 - (pdf.epw * 0.5) / 2, y=60, w=pdf.epw * 0.5)
+image_width = pdf.epw * 0.8
+
+image_x = (pdf.w - image_width) / 2
+
+image_y = 60
+
+pdf.image("shirtificate.png", x=image_x, y=image_y, w=image_width)
+
 
 pdf.set_font("helvetica", "B", 24)
 pdf.set_text_color(255, 255, 255)
 text = f"{user_name} took CS50"
+
 text_width = pdf.get_string_width(text)
-pdf.text(x=(pdf.w - text_width) / 2, y=140, txt=text)
+
+text_x = (pdf.w - text_width) / 2
+
+text_y = image_y + (image_width * 0.5)
+
+
+pdf.text(x=text_x, y=text_y, txt=text)
 
 pdf.output("shirtificate.pdf")
