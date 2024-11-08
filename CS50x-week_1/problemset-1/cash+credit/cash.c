6c90861dff84e15d7c8f6d320168102c51d37e41
Emilj100 <emilbondo12@gmail.com>
2024-11-08 22:00:32 +0000

    Fri, Nov 8, 2024, 11:00 PM +01:00

--- CS50x-week_1/problemset-1/cash+credit/cash.c
+++ CS50x-week_1/problemset-1/cash+credit/cash.c
@@ -74,28 +74,3 @@ int calculate_pennies(int cents)
     }
     return pennies;
 }
-
-
-
-
-
-
-
-
-    // Prompt the user for change owed, in cents
-
-    // Calculate how many quarters you should give customer
-    // Subtract the value of those quarters from cents
-
-    // Calculate how many dimes you should give customer
-    // Subtract the value of those dimes from remaining cents
-
-    // Calculate how many nickels you should give customer
-    // Subtract the value of those nickels from remaining cents
-
-    // Calculate how many pennies you should give customer
-    // Subtract the value of those pennies from remaining cents
-
-    // Sum the number of quarters, dimes, nickels, and pennies used
-    // Print that sum
-
