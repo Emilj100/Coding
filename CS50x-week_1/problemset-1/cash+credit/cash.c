b69ef2c690991fcb33cabf63e1f8eb77de3d2a05
Emilj100 <emilbondo12@gmail.com>
2024-11-08 19:25:01 +0000

    Fri, Nov 8, 2024, 8:25 PM +01:00

--- CS50x-week_1/problemset-1/cash+credit/cash.c
+++ CS50x-week_1/problemset-1/cash+credit/cash.c
@@ -2,4 +2,28 @@
 #include <stdio.h>
 
 int main(void)
+{
+    int user_input = get_int("Change owed: ");
+}
+
+
+
+
+
+    // Prompt the user for change owed, in cents
+
+    // Calculate how many quarters you should give customer
+    // Subtract the value of those quarters from cents
+
+    // Calculate how many dimes you should give customer
+    // Subtract the value of those dimes from remaining cents
+
+    // Calculate how many nickels you should give customer
+    // Subtract the value of those nickels from remaining cents
+
+    // Calculate how many pennies you should give customer
+    // Subtract the value of those pennies from remaining cents
+
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    // Print that sum
 
