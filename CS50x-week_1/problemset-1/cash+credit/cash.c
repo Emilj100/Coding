473f85d4aeb2f913d6517bea8f7668bcc5751d6a
Emilj100 <emilbondo12@gmail.com>
2024-11-08 20:29:58 +0000

    Fri, Nov 8, 2024, 9:29 PM +01:00

--- CS50x-week_1/problemset-1/cash+credit/cash.c
+++ CS50x-week_1/problemset-1/cash+credit/cash.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
@@ -13,8 +16,18 @@ int main(void)
     while (cents < 0);
 
     int quarters = calculate_quarters(cents);
-
     cents -= (quarters * 25);
+
+    int dimes = calculate_dimes(cents);
+    cents -= (dimes * 10);
+
+    int nickels = calculate_nickels(cents);
+    cents -= (nickels * 5);
+
+    int pennies = calculate_pennies(cents);
+    cents -= (pennies * 1);
+
+    printf("%i\n", cents);
 }
 
 
@@ -27,10 +40,42 @@ int calculate_quarters(int cents)
         cents -= 25;
     }
     return quarters;
-
-int calculate_dimes
-
 }
+
+int calculate_dimes(int cents)
+{
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes += 1;
+        cents -= 10;
+    }
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels += 1;
+        cents -= 5;
+    }
+    return nickels;
+}
+
+int calculate_pennies(int cents)
+{
+    int pennies = 0;
+    while (cents >= 1)
+    {
+        pennies += 1;
+        cents -= 1;
+    }
+    return pennies;
+}
+
+
 
 
 
