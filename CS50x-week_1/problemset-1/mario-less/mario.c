d90d3367e2554afb90aacf258935e38564d3bebd
Emilj100 <emilbondo12@gmail.com>
2024-11-08 15:19:59 +0000

    Fri, Nov 8, 2024, 4:19 PM +01:00

--- CS50x-week_1/problemset-1/mario-less/mario.c
+++ CS50x-week_1/problemset-1/mario-less/mario.c
@@ -12,8 +12,10 @@ int main(void)
     while (n < 1);
 
     // Print pyramiden
+    // i starter med at være 0. Loopet skal køre imens i er mindre end n. I hver iteration tilføjer vi +1 til i.
     for (int i = 0; i < n; i += 1)
     {
+        // j starter med at være 0. 
         for (int j = 0; j < n - i - 1; j += 1)
         {
             printf(" ");
