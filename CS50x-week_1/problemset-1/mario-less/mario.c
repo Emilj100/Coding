7c870a52119d3d2c2a38a26970c49b9da3ee75c3
Emilj100 <emilbondo12@gmail.com>
2024-11-08 12:36:56 +0000

    Fri, Nov 8, 2024, 1:36 PM +01:00

--- CS50x-week_1/problemset-1/mario-less/mario.c
+++ CS50x-week_1/problemset-1/mario-less/mario.c
@@ -11,8 +11,10 @@ int main(void)
     }
     while (n < 1);
 
-    for (int i = 0; i < n; i += 1)
+    for (n > 0; i += 1)
     {
+
+
         for (int j = 0; j < n; j += 1)
         {
             printf("#");
