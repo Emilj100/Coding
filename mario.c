c6eb19cb0a6b0434ff8a467e1b104ab882ca4a6a
Emilj100 <emilbondo12@gmail.com>
2024-11-08 08:53:41 +0000

    Fri, Nov 8, 2024, 9:53 AM +01:00

--- mario.c
+++ mario.c
@@ -1,8 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    const int n = 3;
+    const int n = get_int("Size: ");
     for (int i = 0; i < n; i += 1)
     {
         for (int j = 0; j < n; j += 1)
