b1faa3dceec45253c8d4e81f3a8e3b06d02d1c29
Emilj100 <emilbondo12@gmail.com>
2024-11-08 09:37:46 +0000

    Fri, Nov 8, 2024, 10:37 AM +01:00

--- calculator.c
+++ calculator.c
@@ -1,17 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add(int a, int b);
-
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", add(x, y));
+    printf("%i\n", x / y);
 }
 
-int add(int a, int b)
-{
-    return a + b;
-}
