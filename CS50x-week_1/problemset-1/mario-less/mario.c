dcd6c0d507d5ead515c1e6abddc74a033c74f158
Emilj100 <emilbondo12@gmail.com>
2024-11-08 15:29:31 +0000

    Fri, Nov 8, 2024, 4:29 PM +01:00

--- CS50x-week_1/problemset-1/mario-less/mario.c
+++ CS50x-week_1/problemset-1/mario-less/mario.c
@@ -15,11 +15,12 @@ int main(void)
     // i starter med at være 0. Loopet skal køre imens i er mindre end n. I hver iteration tilføjer vi +1 til i.
     for (int i = 0; i < n; i += 1)
     {
-        // j starter med at være 0. Loopet skal derefter køre indtil at imens at j er mindre end n(brugerens input) - i()
+        // j starter med at være 0. Loopet skal derefter køre indtil at imens at j er mindre end n(brugerens input) - i(variablen vi bruger til at afgøre antal linjer)
         for (int j = 0; j < n - i - 1; j += 1)
         {
             printf(" ");
         }
+        // f starter med at være 0. Loopet skal herefter kører imens at f
         for (int f = 0; f < i + 1; f += 1)
         {
             printf("#");
