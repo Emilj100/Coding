7b7fdf17d00b233d7793b90680f0ca08c1d321b1
Emilj100 <emilbondo12@gmail.com>
2024-11-09 21:24:51 +0000

    Sat, Nov 9, 2024, 10:24 PM +01:00

--- hello.c
+++ hello.c
@@ -1,10 +1,15 @@
+#include <cs50.h>
 #include <stdio.h>
+
+void print_column(int height);
 
 int main(void)
 {
-    for (int i = 0; i <= 3; i += 1)
-    {
-        printf("#\n");
-    }
+    int h = get_int("Height: ");
+    print_column(h)
 }
 
+void print_column(int height)
+{
+    printf("#\n");
+}
