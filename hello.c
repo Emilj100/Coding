10505366e41051c6feb265e0f1f567c17b204edb
Emilj100 <emilbondo12@gmail.com>
2024-11-10 17:47:41 +0000

    Sun, Nov 10, 2024, 6:47 PM +01:00

--- hello.c
+++ hello.c
@@ -12,7 +12,15 @@ int main(void)
     for (int i = 0, n = strlen(player_1); i < n; i += 1)
     {
         if (player_1[i] == 'A' || player_1[i] == 'E' || player_1[i] == 'I' || player_1[i] == 'N' || player_1[i] == 'R' || player_1[i] == 'S' || player_1[i] == 'T' || player_1[i] == 'U')
-        counter_1 += 1;
+        {
+            counter_1 += 1;
+        }
+
+        else if (player_1[i] == 'D' || player_1[i] == 'G')
+        {
+            counter_1 += 1;
+        }
+
     }
     printf("%i\n", counter_1);
 
