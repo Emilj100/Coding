e39f441599b83217efdb09dd8a18253bbc203ef1
Emilj100 <emilbondo12@gmail.com>
2024-11-10 09:32:28 +0000

    Sun, Nov 10, 2024, 10:32 AM +01:00

--- hello.c
+++ hello.c
@@ -1,14 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int N = 3;
-
 int main(void)
 {
-    int scores[N];
-    for (int i = 0; i < N; i += 1)
-    {
-        scores[i] = get_int("Score: ");
-    }
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    printf("%c%c%c\n", c1, c2, c3)
 }
