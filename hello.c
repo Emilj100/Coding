626d2f6fa534b76f2776ece81348a3c9c5369aa3
Emilj100 <emilbondo12@gmail.com>
2024-11-10 17:27:27 +0000

    Sun, Nov 10, 2024, 6:27 PM +01:00

--- hello.c
+++ hello.c
@@ -1,16 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-
-int main(int argc, string argv[])
+int main(void)
 {
-    if (argc != 2)
-    {
-        printf("Missing command-line argument\n");
-        return 1;
-    }
-    printf("hello, %s\n", argv[1]);
-    return 0;
+    
 }
-
-
