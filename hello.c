cb58e6a38dba4d15afa96de0124ba0110a32df3c
Emilj100 <emilbondo12@gmail.com>
2024-11-10 17:39:31 +0000

    Sun, Nov 10, 2024, 6:39 PM +01:00

--- hello.c
+++ hello.c
@@ -5,13 +5,13 @@
 
 int main(void)
 {
-    string player_1 = get_string("Input: ");
-    string player_2 = get_string("input ");
+    string player_1 = get_string("Player 1: ");
+    string player_2 = get_string("Player 2: ");
 
     int counter_1 = 0;
     for (int i = 0, n = strlen(player_1); i < n; i += 1)
     {
-        if name
+        if player_1[i] == 'A' 
     }
 
 }
