d03cd595055a01fb12a7f6011d5dcf1383b1d3e6
Emilj100 <emilbondo12@gmail.com>
2024-11-10 10:43:01 +0000

    Sun, Nov 10, 2024, 11:43 AM +01:00

--- hello.c
+++ hello.c
@@ -8,11 +8,16 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i += 1)
     {
-        if (s[i] >= 'a' && <= 'z')
+        if (s[i] >= 'a' && s[i] <= 'z')
         {
-            printf
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
         }
     }
+    printf("\n");
 
 }
 
