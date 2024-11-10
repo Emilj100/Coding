526e8be7c5d15fce1c143e59ef919be06bc56b18
Emilj100 <emilbondo12@gmail.com>
2024-11-10 19:18:29 +0000

    Sun, Nov 10, 2024, 8:18 PM +01:00

--- test.c
+++ test.c
@@ -1,53 +1,44 @@
-#include <ctype.h>
-#include <cs50.h>
 #include <stdio.h>
-#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
 
-// Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+static int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute_score(string s);
 
 int main(void)
 {
-    // Prompt the user for two words
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
-    // Compute the score of each word
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // Print the winner
-    if (score1 > score2)
-    {
-        printf("Player 1 wins!\n");
-    }
-    else if (score1 < score2)
-    {
-        printf("Player 2 wins!\n");
-    }
-    else
+    if (score1 == score2)
     {
         printf("Tie!\n");
     }
+    else if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else
+    {
+        printf("Player 2 wins!\n");
+    }
+
+    return 0;
 }
 
-int compute_score(string word)
+int compute_score(string s)
 {
-    // Keep track of score
     int score = 0;
 
-    // Compute score for each character
-    for (int i = 0, len = strlen(word); i < len; i++)
+    for (int i = 0; s[i] != '\0'; i++)
     {
-        if (isupper(word[i]))
+        if (isalpha(s[i]))
         {
-            score += POINTS[word[i] - 'A'];
-        }
-        else if (islower(word[i]))
-        {
-            score += POINTS[word[i] - 'a'];
+            score += POINTS[tolower(s[i]) - 'a'];
         }
     }
 
