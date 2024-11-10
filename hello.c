413d3667d61749628b04bb2281b8a42c3293a51f
Emilj100 <emilbondo12@gmail.com>
2024-11-10 11:02:05 +0000

    Sun, Nov 10, 2024, 12:02 PM +01:00

--- hello.c
+++ hello.c
@@ -4,13 +4,9 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    for (int i = 0; i < argc; i += 1)
     {
-        printf("hello, %s\n", argv[1]);
-    }
-    else
-    {
-        printf("hello, world\n");
+        printf("%s\n", argv[i]);
     }
 
 }
