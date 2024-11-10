5d5323ba429d4e89d166473be4a78d6279ddbfde
Emilj100 <emilbondo12@gmail.com>
2024-11-10 17:59:48 +0000

    Sun, Nov 10, 2024, 6:59 PM +01:00

--- hello.c
+++ hello.c
@@ -5,40 +5,43 @@
 
 int main(void)
 {
-    string player_1 = get_string("Player 1: ");
-    string player_2 = get_string("Player 2: ");
+    string player[0] = get_string("Player 1: ");
+    string player[1] = get_string("Player 2: ");
 
     int counter_1 = 0;
-    for (int i = 0, n = strlen(player_1); i < n; i += 1)
+    for (int number = 0; number < 2; number += 1)
     {
-        if (player_1[i] == 'A' || player_1[i] == 'E' || player_1[i] == 'I' || player_1[i] == 'N' || player_1[i] == 'R' || player_1[i] == 'S' || player_1[i] == 'T' || player_1[i] == 'U')
+        for (int i = 0, n = strlen(player_1); i < n; i += 1)
         {
-            counter_1 += 1;
-        }
-
-        else if (player_1[i] == 'D' || player_1[i] == 'G')
-        {
-            counter_1 += 2;
-        }
-        else if (player_1[i] == 'B' || player_1[i] == 'C' || player_1[i] == 'M' || player_1[i] == 'P')
-        {
-            counter_1 += 3;
-        }
-        else if (player_1[i] == 'F' || player_1[i] == 'H' || player_1[i] == 'V' || player_1[i] == 'W' || player_1[i] == 'Y')
-        {
-            counter_1 += 4;
-        }
-        else if (player_1[i] == 'K')
-        {
-            counter_1 += 5;
-        }
-        else if (player_1[i] == 'J' || player_1[i] == 'X')
-        {
-            counter_1 += 8;
-        }
-        else if (player_1[i] == 'Q' || player_1[i] == 'Z')
-        {
-            counter_1 += 10;
+            if (player_1[number] == 'A' || player_1[number] == 'E' || player_1[number] == 'I' || player_1[number] == 'N' || player_1[number] == 'R'
+             || player_1[number] == 'S' || player_1[number] == 'T' || player_1[number] == 'U')
+            {
+                counter_1 += 1;
+            }
+            else if (player_1[number] == 'D' || player_1[number] == 'G')
+            {
+                counter_1 += 2;
+            }
+            else if (player_1[number] == 'B' || player_1[number] == 'C' || player_1[number] == 'M' || player_1[number] == 'P')
+            {
+                counter_1 += 3;
+            }
+            else if (player_1[number] == 'F' || player_1[number] == 'H' || player_1[number] == 'V' || player_1[number] == 'W' || player_1[number] == 'Y')
+            {
+                counter_1 += 4;
+            }
+            else if (player_1[number] == 'K')
+            {
+                counter_1 += 5;
+            }
+            else if (player_1[number] == 'J' || player_1[number] == 'X')
+            {
+                counter_1 += 8;
+            }
+            else if (player_1[number] == 'Q' || player_1[number] == 'Z')
+            {
+                counter_1 += 10;
+            }
         }
 
     }
