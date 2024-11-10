0767d41331acc3ca1eaf54420f1405d050ed1987
Emilj100 <emilbondo12@gmail.com>
2024-11-10 18:05:45 +0000

    Sun, Nov 10, 2024, 7:05 PM +01:00

--- hello.c
+++ hello.c
@@ -5,41 +5,41 @@
 
 int main(void)
 {
-    
-    string player[0] = get_string("Player 1: ");
-    string player[1] = get_string("Player 2: ");
+    string player[2];
+    player[0] = get_string("Player 1: ");
+    player[1] = get_string("Player 2: ");
 
     int counter_1 = 0;
     for (int number = 0; number < 2; number += 1)
     {
-        for (int i = 0, n = strlen(player_1); i < n; i += 1)
+        for (int i = 0, n = strlen(player[number]); i < n; i += 1)
         {
-            if (player_1[number] == 'A' || player_1[number] == 'E' || player_1[number] == 'I' || player_1[number] == 'N' || player_1[number] == 'R'
-             || player_1[number] == 'S' || player_1[number] == 'T' || player_1[number] == 'U')
+            if (player[number] == 'A' || player[number] == 'E' || player[number] == 'I' || player[number] == 'N' || player[number] == 'R'
+             || player[number] == 'S' || player[number] == 'T' || player[number] == 'U')
             {
                 counter_1 += 1;
             }
-            else if (player_1[number] == 'D' || player_1[number] == 'G')
+            else if (player[number] == 'D' || player[number] == 'G')
             {
                 counter_1 += 2;
             }
-            else if (player_1[number] == 'B' || player_1[number] == 'C' || player_1[number] == 'M' || player_1[number] == 'P')
+            else if (player[number] == 'B' || player[number] == 'C' || player[number] == 'M' || player[number] == 'P')
             {
                 counter_1 += 3;
             }
-            else if (player_1[number] == 'F' || player_1[number] == 'H' || player_1[number] == 'V' || player_1[number] == 'W' || player_1[number] == 'Y')
+            else if (player[number] == 'F' || player[number] == 'H' || player[number] == 'V' || player[number] == 'W' || player[number] == 'Y')
             {
                 counter_1 += 4;
             }
-            else if (player_1[number] == 'K')
+            else if (player[number] == 'K')
             {
                 counter_1 += 5;
             }
-            else if (player_1[number] == 'J' || player_1[number] == 'X')
+            else if (player[number] == 'J' || player[number] == 'X')
             {
                 counter_1 += 8;
             }
-            else if (player_1[number] == 'Q' || player_1[number] == 'Z')
+            else if (player[number] == 'Q' || player[number] == 'Z')
             {
                 counter_1 += 10;
             }
