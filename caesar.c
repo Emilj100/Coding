c673a6a93191d803284c43d7ac69f1585c27c093
Emilj100 <emilbondo12@gmail.com>
2024-11-11 13:26:13 +0000

    Mon, Nov 11, 2024, 2:26 PM +01:00

--- caesar.c
+++ caesar.c
@@ -16,7 +16,7 @@ int main(int argc, string argv[])
             string plaintext = get_string("plaintext:  ");
             for (int i = 0, n = strlen(plaintext); i < n; i += 1)
             {
-                int rotate = 0;
+                string rotate;
                 if (isupper(plaintext[i]))
                 {
                     rotate += ((plaintext[i] - 'A' + key) % 26) + 'A';
@@ -30,12 +30,10 @@ int main(int argc, string argv[])
                     rotate += plaintext[i];
                 }
 
-                printf("ciphertext: %s", rotate);
-
-            printf("\n");
-            return 0;
-
+                printf("%s", rotate);
             }
+
+
         }
 
     }
