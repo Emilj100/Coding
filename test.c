50c2043a06586264057e475a7ec99fd4fccd9757
Emilj100 <emilbondo12@gmail.com>
2024-11-11 10:20:20 +0000

    Mon, Nov 11, 2024, 11:20 AM +01:00

--- test.c
+++ test.c
@@ -1,88 +1,90 @@
+#include <ctype.h>
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
+// Function prototypes
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
-int getGradeLevel(int letters, int words, int sentences);
+int calculate_index(int letters, int words, int sentences);
 
 int main(void)
 {
+    // Prompt the user for some text
     string text = get_string("Text: ");
 
-    int cl = count_letters(text),
-        cw = count_words(text),
-        cs = count_sentences(text);
+    // Count letters, words, and sentences
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
-    int grade_level = getGradeLevel(cl, cw, cs);
+    // Calculate readability index
+    int index = calculate_index(letters, words, sentences);
 
-    if (grade_level < 1)
+    // Print the grade level
+    if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if (grade_level >= 16)
+    else if (index >= 16)
     {
         printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade %d\n", grade_level);
+        printf("Grade %i\n", index);
     }
-
-    return 0;
 }
 
+// Function to count letters in the text
 int count_letters(string text)
 {
-    int count = 0;
-
+    int letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isalpha(text[i]))
         {
-            count++;
+            letters++;
         }
     }
-
-    return count;
+    return letters;
 }
 
+// Function to count words in the text
 int count_words(string text)
 {
-    int count = 1;
-
+    int words = 1;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isspace(text[i]))
         {
-            count++;
+            words++;
         }
     }
-
-    return count;
+    return words;
 }
 
+// Function to count sentences in the text
 int count_sentences(string text)
 {
-    int count = 0;
-
+    int sentences = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
-            count++;
+            sentences++;
         }
     }
-
-    return count;
+    return sentences;
 }
 
-int getGradeLevel(int letters, int words, int sentences)
+// Function to calculate the Coleman-Liau index
+int calculate_index(int letters, int words, int sentences)
 {
     float L = (float) letters / words * 100;
     float S = (float) sentences / words * 100;
-
     return round(0.0588 * L - 0.296 * S - 15.8);
 }
+
