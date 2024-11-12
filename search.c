c6f2fa66b7ccffccffc2254a2f3f07d62016c79a
Emilj100 <emilbondo12@gmail.com>
2024-11-12 10:41:00 +0000

    Tue, Nov 12, 2024, 11:41 AM +01:00

--- search.c
+++ search.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50}
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
     int n = get_int("Number: ");
     for (int i = 0; i < 7; i += 1)
