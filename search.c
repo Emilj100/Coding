82e6da1062a9dd92ec81d517f094209bc692e419
Emilj100 <emilbondo12@gmail.com>
2024-11-12 10:51:33 +0000

    Tue, Nov 12, 2024, 11:51 AM +01:00

--- search.c
+++ search.c
@@ -3,17 +3,5 @@
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
-
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i += 1)
-    {
-        if (numbers[i] == n)
-        {
-            printf("Found\n");
-            return 0;
-        }
-    }
-    printf("Not found\n");
-    return 1;
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 }
