5faca885de03bd707abd4558421e1aa187df182f
Emilj100 <emilbondo12@gmail.com>
2024-11-12 10:53:56 +0000

    Tue, Nov 12, 2024, 11:53 AM +01:00

--- search.c
+++ search.c
@@ -4,4 +4,16 @@
 int main(void)
 {
     string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i += 1)
+    {
+        if (strings[i] == s)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
