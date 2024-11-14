5a14994685f2f6afe55a1bc6bd029676401c4786
Emilj100 <emilbondo12@gmail.com>
2024-11-14 17:55:45 +0000

    Thu, Nov 14, 2024, 6:55 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,11 +130,10 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            
+            preferences[voter][rank] = i;
+            return true;
         }
     }
-
-
     return false;
 }
 
