34aec9bdfacf699654c3da384d9a39d8d8e03bdd
Emilj100 <emilbondo12@gmail.com>
2024-11-14 17:47:50 +0000

    Thu, Nov 14, 2024, 6:47 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -126,7 +126,15 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i += 1)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            
+        }
+    }
+
+
     return false;
 }
 
