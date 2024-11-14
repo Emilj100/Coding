ef197525c5c944ede9ec874ae681316058086c65
Emilj100 <emilbondo12@gmail.com>
2024-11-14 13:17:16 +0000

    Thu, Nov 14, 2024, 2:17 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,14 +65,15 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-
-    if (strcmp(candidates[i].name, name) == 0)
+    for (int i = 0; i < candidate_count; i += 1)
     {
-        candidates[i].votes += 1;
-        return true;
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes += 1;
+            return true;
+        }
     }
-}
-return false;
+    return false;
 
 }
 
