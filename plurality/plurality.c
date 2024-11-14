9cbcc63cfbe3c63c9ee20caebb35d16e464d657c
Emilj100 <emilbondo12@gmail.com>
2024-11-14 10:18:38 +0000

    Thu, Nov 14, 2024, 11:18 AM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -84,7 +84,14 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    int max_votes = 0;
     for (int i = 0; i < candidate_count; i += 1)
+    {
+        if (votes_counter[i] > max_votes)
+        {
+            max_votes = votes_counter[i];
+        }
+    }
 
     return;
 }
