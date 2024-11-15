c2b8c3e423ace9e58adc76f45dd936f2b756b911
Emilj100 <emilbondo12@gmail.com>
2024-11-15 12:54:30 +0000

    Fri, Nov 15, 2024, 1:54 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -99,7 +99,7 @@ int main(int argc, string argv[])
         int min = find_min();
         // For at teste find_min funktionen
         printf("%i\n", min);
-        break;
+        return 8;
         bool tie = is_tie(min);
 
         // If tie, everyone wins
