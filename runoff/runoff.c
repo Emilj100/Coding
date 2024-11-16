71be5f0e965d7ede1961ca7cdcc461ea48f14e07
Emilj100 <emilbondo12@gmail.com>
2024-11-16 10:43:18 +0000

    Sat, Nov 16, 2024, 11:43 AM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -197,7 +197,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i += 1)
+    {
+        if (!candidates[i].eliminated)
+    }
+
     return false;
 }
 
