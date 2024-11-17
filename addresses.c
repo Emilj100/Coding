6fd1c3eced8f221d8c0c9a6efad1090398025c40
Emilj100 <emilbondo12@gmail.com>
2024-11-17 14:22:33 +0000

    Sun, Nov 17, 2024, 3:22 PM +01:00

--- addresses.c
+++ addresses.c
@@ -4,10 +4,6 @@
 int main(void)
 {
     string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    printf("%s\n", s);
 
 }
