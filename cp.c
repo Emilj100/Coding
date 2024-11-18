72de8ab68076b6de095549407e4bb364e281f6bb
Emilj100 <emilbondo12@gmail.com>
2024-11-18 18:48:54 +0000

    Mon, Nov 18, 2024, 7:48 PM +01:00

---
+++ cp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+
+    BYTE b;
+
+}
