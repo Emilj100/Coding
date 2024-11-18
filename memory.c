75c6e6048160f52f051e004247023650af94b7a3
Emilj100 <emilbondo12@gmail.com>
2024-11-18 13:25:15 +0000

    Mon, Nov 18, 2024, 2:25 PM +01:00

--- memory.c
+++ memory.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int scores[1024];
-    for (int i = 0; i < 1024; i += 1)
-    {
-        printf("%i\n", scores[i]);
-    }
+    int *x = malloc(3 * sizeof(int));
+    x[0] = 72;
+    x[1] = 73;
+    x[2] = 33;
+    free(x);
 }
