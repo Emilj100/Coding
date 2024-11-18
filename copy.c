f4a1f0743afa222b45b7d9bcb290475a293f2630
Emilj100 <emilbondo12@gmail.com>
2024-11-18 10:18:37 +0000

    Mon, Nov 18, 2024, 11:18 AM +01:00

--- copy.c
+++ copy.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
-    char *t = s;
+
+    char *t = malloc(strlen(s) + 1);
+
+    for (int i = 0, n = strlen(s); i < n i += 1)
+    {
+        t[i] = s[i];
+    }
 
     t[1] = toupper(t[1]);
 
