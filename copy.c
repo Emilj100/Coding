7feeb5d79b04a6f2eb2154cc4ae9e42e95dd0b61
Emilj100 <emilbondo12@gmail.com>
2024-11-18 10:20:21 +0000

    Mon, Nov 18, 2024, 11:20 AM +01:00

--- copy.c
+++ copy.c
@@ -10,12 +10,15 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for (int i = 0, n = strlen(s); i < n i += 1)
+    for (int i = 0, n = strlen(s); i <= n i += 1)
     {
         t[i] = s[i];
     }
 
-    t[1] = toupper(t[1]);
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf("%s\n", s);
     printf("%s\n", t);
