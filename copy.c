f930ae5ace5c7f53e687a4c7a7955d2068f5097c
Emilj100 <emilbondo12@gmail.com>
2024-11-18 10:46:38 +0000

    Mon, Nov 18, 2024, 11:46 AM +01:00

--- copy.c
+++ copy.c
@@ -9,6 +9,10 @@ int main(void)
     char *s = get_string("s: ");
 
     char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
 
     for (int i = 0, n = strlen(s); i <= n i += 1)
     {
