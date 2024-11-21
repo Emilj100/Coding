2a30017050f6a469b3d36b46e3bc41f69d017c86
Emilj100 <emilbondo12@gmail.com>
2024-11-21 15:33:09 +0000

    Thu, Nov 21, 2024, 4:33 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -91,6 +91,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             copy[i][j] = image[i][j];
+            
         }
     }
     return;
