29024cc0dfd41d83b5467372585a9c9107a77734
Emilj100 <emilbondo12@gmail.com>
2024-11-21 14:40:18 +0000

    Thu, Nov 21, 2024, 3:40 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -72,7 +72,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Swap pixels
+            int center = width / 2;
+            printf("%i\n", center)
         }
     }
     return;
