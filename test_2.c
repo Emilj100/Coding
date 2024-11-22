18fc5a63152df28661baa3b1df249ccb852ba23f
Emilj100 <emilbondo12@gmail.com>
2024-11-22 18:04:00 +0000

    Fri, Nov 22, 2024, 7:04 PM +01:00

--- test_2.c
+++ test_2.c
@@ -1,6 +1,8 @@
 #include "helpers.h"
-#include <stdio.h>
 #include <math.h>
+
+int threshold(int value);
+
 
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -8,37 +10,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Beregn gennemsnittet af RGB-værdierne
-            int avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            int gray = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
 
-            // Sæt alle RGB-værdier til gennemsnittet
-            image[i][j].rgbtRed = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtBlue = avg;
-        }
-    }
-}
-
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // Gem de oprindelige RGB-værdier
-            int originalRed = image[i][j].rgbtRed;
-            int originalGreen = image[i][j].rgbtGreen;
-            int originalBlue = image[i][j].rgbtBlue;
-
-            // Beregn sepia RGB-værdier
-            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
-            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
-
-            // Sørg for, at værdierne ikke overstiger 255
-            image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
-            image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
-            image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
         }
     }
 }
@@ -49,57 +25,106 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width / 2; j++)
         {
-            // Swap pixel på venstre og højre side
             RGBTRIPLE temp = image[i][j];
-            image[i][j] = image[i][width - j - 1];
-            image[i][width - j - 1] = temp;
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
         }
     }
 }
 
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Lav en kopi af billedet
-    RGBTRIPLE copy[height][width];
+    RGBTRIPLE temp[height][width];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            copy[i][j] = image[i][j];
-        }
-    }
-
-    // Gennemgå hver pixel i billedet
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int redSum = 0, greenSum = 0, blueSum = 0;
             int count = 0;
 
-            // Iterér gennem nabolaget
-            for (int di = -1; di <= 1; di++)
+            // The blurring kernel
+            for (int k = -1; k <= 1; k++)
             {
-                for (int dj = -1; dj <= 1; dj++)
+                for (int l = -1; l <= 1; l++)
                 {
-                    int ni = i + di;
-                    int nj = j + dj;
+                    int newI = i + k;
+                    int newJ = j + l;
 
-                    // Tjek om naboen er inden for billedets grænser
-                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    if (newI >= 0 && newI < height && newJ >= 0 && newJ < width)
                     {
-                        totalRed += copy[ni][nj].rgbtRed;
-                        totalGreen += copy[ni][nj].rgbtGreen;
-                        totalBlue += copy[ni][nj].rgbtBlue;
+                        redSum += image[newI][newJ].rgbtRed;
+                        greenSum += image[newI][newJ].rgbtGreen;
+                        blueSum += image[newI][newJ].rgbtBlue;
                         count++;
                     }
                 }
             }
 
-            // Beregn gennemsnittet
-            image[i][j].rgbtRed = (totalRed + count / 2) / count;
-            image[i][j].rgbtGreen = (totalGreen + count / 2) / count;
-            image[i][j].rgbtBlue = (totalBlue + count / 2) / count;
+            temp[i][j].rgbtRed = round((float)redSum / count);
+            temp[i][j].rgbtGreen = round((float)greenSum / count);
+            temp[i][j].rgbtBlue = round((float)blueSum / count);
         }
     }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+}
+
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int GxRed = 0, GxGreen = 0, GxBlue = 0;
+            int GyRed = 0, GyGreen = 0, GyBlue = 0;
+
+            for (int k = -1; k <= 1; k++)
+            {
+                for (int l = -1; l <= 1; l++)
+                {
+                    int newI = i + k;
+                    int newJ = j + l;
+
+                    if (newI >= 0 && newI < height && newJ >= 0 && newJ < width)
+                    {
+                        GxRed += Gx[k + 1][l + 1] * image[newI][newJ].rgbtRed;
+                        GxGreen += Gx[k + 1][l + 1] * image[newI][newJ].rgbtGreen;
+                        GxBlue += Gx[k + 1][l + 1] * image[newI][newJ].rgbtBlue;
+
+                        GyRed += Gy[k + 1][l + 1] * image[newI][newJ].rgbtRed;
+                        GyGreen += Gy[k + 1][l + 1] * image[newI][newJ].rgbtGreen;
+                        GyBlue += Gy[k + 1][l + 1] * image[newI][newJ].rgbtBlue;
+                    }
+                }
+            }
+
+            temp[i][j].rgbtRed = threshold(round(sqrt(GxRed * GxRed + GyRed * GyRed)));
+            temp[i][j].rgbtGreen = threshold(round(sqrt(GxGreen * GxGreen + GyGreen * GyGreen)));
+            temp[i][j].rgbtBlue = threshold(round(sqrt(GxBlue * GxBlue + GyBlue * GyBlue)));
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+}
+
+int threshold(int value)
+{
+    return value > 255 ? 255 : (value < 0 ? 0 : value);
 }
