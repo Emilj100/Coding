d3624001bbc325ddf417f2297ee301a21f1abedc
Emilj100 <emilbondo12@gmail.com>
2024-11-22 17:47:39 +0000

    Fri, Nov 22, 2024, 6:47 PM +01:00

--- test_2.c
+++ test_2.c
@@ -1,15 +1,105 @@
-            // For hver pixel i rækken iterer over dens naboer
-            for (int x = 0; x < height x++)
+#include "helpers.h"
+#include <stdio.h>
+#include <math.h>
+
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Beregn gennemsnittet af RGB-værdierne
+            int avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+
+            // Sæt alle RGB-værdier til gennemsnittet
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
+}
+
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Gem de oprindelige RGB-værdier
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            // Beregn sepia RGB-værdier
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            // Sørg for, at værdierne ikke overstiger 255
+            image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
+            image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+            image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+        }
+    }
+}
+
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap pixel på venstre og højre side
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
+        }
+    }
+}
+
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Lav en kopi af billedet
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Gennemgå hver pixel i billedet
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int count = 0;
+
+            // Iterér gennem nabolaget
+            for (int di = -1; di <= 1; di++)
             {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
 
-
-
+                    // Tjek om naboen er inden for billedets grænser
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        totalRed += copy[ni][nj].rgbtRed;
+                        totalGreen += copy[ni][nj].rgbtGreen;
+                        totalBlue += copy[ni][nj].rgbtBlue;
+                        count++;
+                    }
+                }
             }
-            for (int y = 0; y < width y++)
-            {
 
-            }
-
-            copy[i][j].rgbtRed = main_pixel_red;
-            copy[i][j].rgbtGreen = main_pixel_green;
-            copy[i][j].rgbtBlue = main_pixel_blue;
+            // Beregn gennemsnittet
+            image[i][j].rgbtRed = (totalRed + count / 2) / count;
+            image[i][j].rgbtGreen = (totalGreen + count / 2) / count;
+            image[i][j].rgbtBlue = (totalBlue + count / 2) / count;
+        }
+    }
+}
