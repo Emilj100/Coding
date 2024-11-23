744e9fa2e2c8748eb0f8d98de382842dff4d3d20
Emilj100 <emilbondo12@gmail.com>
2024-11-23 11:51:20 +0000

    Sat, Nov 23, 2024, 12:51 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
- 
+     // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+    // Create a buffer for a block of data
+    BYTE buffer[512];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+
+    }
 }
