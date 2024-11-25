b0436642f898d3cba022106ea48cb6d6bd408de9
Emilj100 <emilbondo12@gmail.com>
2024-11-25 09:53:57 +0000

    Mon, Nov 25, 2024, 10:53 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,8 +31,10 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // Dette er starten på en ny JPEG-fil
+            int i = 0;
             sprintf(filename, "%03i.jpeg", i);
             FILE *file = fopen(filename, "a");
+            i += 1;
 
         }
 
