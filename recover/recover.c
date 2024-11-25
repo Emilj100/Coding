ac36e7af644e4b1a68c7dfae0b302e91a43313ca
Emilj100 <emilbondo12@gmail.com>
2024-11-25 13:19:39 +0000

    Mon, Nov 25, 2024, 2:19 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,14 +8,12 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover FILE\n");
         return 1;
     }
 
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
-        printf("Could not open file.\n");
         return 1;
     }
 
