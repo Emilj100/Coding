3a9521927c3d27304feb23f6c7de185466751577
Emilj100 <emilbondo12@gmail.com>
2024-11-25 12:34:38 +0000

    Mon, Nov 25, 2024, 1:34 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,7 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-     // Accept a single command-line argument
+    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: ./recover FILE\n");
@@ -17,28 +17,54 @@ int main(int argc, char *argv[])
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
+        printf("Could not open file.\n");
         return 1;
     }
 
     // Create a buffer for a block of data
     BYTE buffer[512];
     char filename[9]; // plads til "000.jpeg" + null terminator
+    FILE *currentfile = NULL; // Pointer til den åbne JPEG-fil
+    int fileIndex = 0;        // Tæller for JPEG-filer
 
     // While there's still data left to read from the memory card
-    int i = 0;
     while (fread(buffer, 1, 512, card) == 512)
     {
-            sprintf(filename, "%03i.jpg", i);
-            FILE *file = fopen(filename, "w");
-            fwrite(buffer, 1, 512, file);
-        // Create JPEGs from the data
+        // Check if this block is the start of a new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // Dette er starten på en ny JPEG-fil
-            fclose(file);
-            i += 1;
+            // If a file is already open, close it
+            if (currentfile != NULL)
+            {
+                fclose(currentfile);
+            }
 
+            // Create a new file
+            sprintf(filename, "%03i.jpg", fileIndex);
+            currentfile = fopen(filename, "w");
+            if (currentfile == NULL)
+            {
+                printf("Could not create file %s.\n", filename);
+                return 1;
+            }
+            fileIndex++;
         }
 
+        // Write to the currently open file, if any
+        if (currentfile != NULL)
+        {
+            fwrite(buffer, 1, 512, currentfile);
+        }
     }
+
+    // Close any remaining open file
+    if (currentfile != NULL)
+    {
+        fclose(currentfile);
+    }
+
+    // Close the memory card file
+    fclose(card);
+
+    return 0;
 }
