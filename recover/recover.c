6b54b00ae7deb948366b722aa829edd9c6cfe99a
Emilj100 <emilbondo12@gmail.com>
2024-11-25 13:16:38 +0000

    Mon, Nov 25, 2024, 2:16 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,14 +6,12 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: ./recover FILE\n");
         return 1;
     }
 
-    // Open the memory card
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
@@ -21,44 +19,38 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Create a buffer for a block of data
     BYTE buffer[512];
-    char filename[9]; // plads til "000.jpeg" + null terminator
-    FILE *currentfile = NULL; // Pointer til den åbne JPEG-fil
-    int fileIndex = 0;        // Tæller for JPEG-filer
+    char filename[9];
+    FILE *currentfile = NULL;
+    int fileIndex = 0;
 
-    // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
-        // Check if this block is the start of a new JPEG
+
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If a file is already open, close it
+
             if (currentfile != NULL)
             {
                 fclose(currentfile);
             }
 
-            // Create a new file
             sprintf(filename, "%03i.jpg", fileIndex);
             currentfile = fopen(filename, "w");
             fileIndex += 1;
         }
 
-        // Write to the currently open file, if any
         if (currentfile != NULL)
         {
             fwrite(buffer, 1, 512, currentfile);
         }
     }
 
-    // Close any remaining open file
     if (currentfile != NULL)
     {
         fclose(currentfile);
     }
 
-    // Close the memory card file
     fclose(card);
 
     return 0;
