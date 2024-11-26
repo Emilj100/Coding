68ab1b32ad4c17ca2c5afb40d6b08c0163cefa71
Emilj100 <emilbondo12@gmail.com>
2024-11-26 12:49:56 +0000

    Tue, Nov 26, 2024, 1:49 PM +01:00

--- list.c
+++ list.c
@@ -8,5 +8,31 @@ typedef struct node
 } node;
 
 int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            // Free memory thus far
+            return 1;
+        }
+        n->number = number;
+        n->next = list;
+        list = n;
+    }
+
+    // Print whole list
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
+}
 
 
