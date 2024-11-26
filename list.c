6bea049b3f49f078b47ff5e8a4cc3b258ab3a425
Emilj100 <emilbondo12@gmail.com>
2024-11-26 12:38:48 +0000

    Tue, Nov 26, 2024, 1:38 PM +01:00

--- list.c
+++ list.c
@@ -1,46 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
-    {
-        return 1;
-    }
-
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = malloc (4 * sizeof(int));
-    if (tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-
-
-    for (int i = 0; i < 2; i++)
-    {
-        tmp[i] = list[i];
-    }
-    tmp[3] = 4;
-
-    free(list);
-    list = tmp;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
-    return 0;
-}
-
 typedef struct node
 {
     int number;
     struct node *next;
 } node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+}
