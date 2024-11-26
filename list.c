b8167d874191ac42168c20febdb29125ab638016
Emilj100 <emilbondo12@gmail.com>
2024-11-26 13:41:32 +0000

    Tue, Nov 26, 2024, 2:41 PM +01:00

--- list.c
+++ list.c
@@ -1,5 +1,8 @@
+// Implements a list of numbers using a linked list
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
 
 typedef struct node
 {
@@ -9,29 +12,46 @@ typedef struct node
 
 int main(int argc, char *argv[])
 {
+    // Memory for numbers
     node *list = NULL;
 
+    // For each command-line argument
     for (int i = 1; i < argc; i++)
     {
+        // Convert argument to int
         int number = atoi(argv[i]);
 
+        // Allocate node for number
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
-            // Free memory thus far
+
             return 1;
         }
         n->number = number;
-        n->next = list;
-        list = n;
-    }
+        n->next = NULL;
+        // If list is empty
+        if (list == NULL)
+        {
+            // This node is the whole list
+            list = n;
+        }
 
-    // Print whole list
-    node *ptr = list;
-    while (ptr != NULL)
-    {
-        printf("%i\n", ptr->number);
-        ptr = ptr->next;
+        // If list has numbers already
+        else
+        {
+
+            // Iterate over nodes in list
+            for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                if (ptr->next == NULL)
+                {
+                    // Append node
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
     }
 }
 
