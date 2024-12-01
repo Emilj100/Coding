90e3f91ab8af6d43fc39964e292b51408ca28ce1
Emilj100 <emilbondo12@gmail.com>
2024-12-01 10:04:30 +0000

    Sun, Dec 1, 2024, 11:04 AM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -70,7 +70,6 @@ bool load(const char *dictionary)
         }
         strcpy(n->word, buffer);
         int hashvalue = hash(n->word);
-        printf("Indlæser ord: %s i bucket: %i\n", buffer, hashvalue);
         n->next = table[hashvalue];
         table[hashvalue] = n;
 
