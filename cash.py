00c829ad7b61bf404d67bcab8cc4d107707b7e3c
Emilj100 <emilbondo12@gmail.com>
2024-12-03 19:25:14 +0000

    Tue, Dec 3, 2024, 8:25 PM +01:00

--- cash.py
+++ cash.py
@@ -1,31 +1,37 @@
+def main():
+    while True:
+        try:
+            dollars = float(input("Change: "))
+            if dollars > 0:
+                break
+        except ValueError:
+            print("Not an integer")
+    coins(dollars)
 
-while True:
-    try:
-        dollars = float(input("Change: "))
-        if dollars > 0:
-            break
-    except ValueError:
-        print("Not an integer")
+def coins(dollars):
 
-cents = int(round(dollars * 100))
-coins = 0
+    cents = int(round(dollars * 100))
+    coins = 0
 
-while cents > 0:
+    while cents > 0:
 
-    if cents >= 25:
-        coins += 1
-        cents -= 25
+        if cents >= 25:
+            coins += 1
+            cents -= 25
 
-    elif cents >= 10:
-        coins += 1
-        cents -= 10
+        elif cents >= 10:
+            coins += 1
+            cents -= 10
 
-    elif cents >= 5:
-        coins += 1
-        cents -= 5
+        elif cents >= 5:
+            coins += 1
+            cents -= 5
 
-    elif cents >= 1:
-        coins += 1
-        cents -= 1
+        elif cents >= 1:
+            coins += 1
+            cents -= 1
 
-print(coins)
+    print(coins)
+
+if __name__ == "__main__":
+    main()
