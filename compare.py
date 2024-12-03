7de66cb0d13adde74ee6ac8db465d2a4ff803227
Emilj100 <emilbondo12@gmail.com>
2024-12-03 10:45:55 +0000

    Tue, Dec 3, 2024, 11:45 AM +01:00

--- compare.py
+++ compare.py
@@ -1,9 +1,7 @@
-x = int(input("What's x? "))
-y = int(input("What's x? "))
+s = input("s: ")
+t = input("t: ")
 
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
+if s == t:
+    print("Same")
 else:
-    print("x is equal to y")
+    print("Different")
