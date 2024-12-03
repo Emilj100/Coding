631fa47f127717ec3d470f1b872c94eba7e0c5e2
Emilj100 <emilbondo12@gmail.com>
2024-12-03 13:42:59 +0000

    Tue, Dec 3, 2024, 2:42 PM +01:00

--- mario.py
+++ mario.py
@@ -1,12 +1,1 @@
-while True:
-    try:
-        n = int(input("Length: "))
-        if n > 0:
-            break
-    except ValueError:
-        pass
-
-for i in range(n):
-    print("?", end="")
-print()
-
+print("?" * 4)
