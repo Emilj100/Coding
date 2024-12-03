ccc35dc03db732883c6c9a9a2426c3fa6673563f
Emilj100 <emilbondo12@gmail.com>
2024-12-03 17:33:39 +0000

    Tue, Dec 3, 2024, 6:33 PM +01:00

--- mario.py
+++ mario.py
@@ -1,8 +1,10 @@
 n = int(input("Size: "))
 
+x = 0
 for _ in range(n):
+    x += 1
     for _ in range(n):
         print(" ", end="")
-    for _ in range():
+    for _ in range(x):
         print("#", end="")
     print()
