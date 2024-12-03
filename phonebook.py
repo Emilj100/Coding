20db044d029812541f96230fff5a4676b819a51b
Emilj100 <emilbondo12@gmail.com>
2024-12-03 15:21:52 +0000

    Tue, Dec 3, 2024, 4:21 PM +01:00

--- phonebook.py
+++ phonebook.py
@@ -8,8 +8,7 @@ name = input("Name: ")
 
 for person in people:
     if person["name"] == name:
-        number = person["number"]
-        print(f"Found {number}")
+        print(f"Found {person['number']}")
         break
 
 else:
