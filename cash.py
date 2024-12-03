d9d5031974e8e52ff460acf456dfc3b55d0fa35c
Emilj100 <emilbondo12@gmail.com>
2024-12-03 18:45:41 +0000

    Tue, Dec 3, 2024, 7:45 PM +01:00

--- cash.py
+++ cash.py
@@ -2,18 +2,22 @@ dollars = int(input("Change: "))
 
 coins = 0
 
-while dollars < 0
+while dollars > 0:
 
-if dollars >= 25:
-    coins += 1
+    if dollars >= 25:
+        coins += 1
+        dollars -= 25
 
-elif dollars >= 10:
-    coins += 1
+    elif dollars >= 10:
+        coins += 1
+        dollars -= 10
 
-elif dollars >= 5:
-    coins += 1
+    elif dollars >= 5:
+        coins += 1
+        dollars -= 5
 
-elif dollars >= 1:
-    coins += 1
+    elif dollars >= 1:
+        coins += 1
+        dollars -= 1
 
 print(coins)
