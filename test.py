10bd038ff7aeb2beaaaec951104b9add19af7091
Emilj100 <emilbondo12@gmail.com>
2024-12-03 19:37:06 +0000

    Tue, Dec 3, 2024, 8:37 PM +01:00

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
 def main():
-    # Indhent gyldig højde fra brugeren
-    height = get_height()
+    # Indhent gyldigt input fra brugeren
+    cents = get_cents()
 
-    # Byg pyramiden
-    for i in range(1, height + 1):
-        # Print mellemrum for højrejustering
-        print(" " * (height - i), end="")
-        # Print hashes
-        print("#" * i)
+    # Beregn og print antallet af mønter
+    coins = calculate_coins(cents)
+    print(coins)
 
 
-def get_height():
-    """Indhenter en gyldig højde fra brugeren mellem 1 og 8."""
+def get_cents():
+    """Indhenter gyldigt beløb i dollars og konverterer det til cent."""
     while True:
         try:
-            height = int(input("Height: "))
-            if 1 <= height <= 8:
-                return height
+            dollars = float(input("Change owed: "))
+            if dollars > 0:
+                return int(round(dollars * 100))
         except ValueError:
-            pass  # Hvis brugeren ikke indtaster et tal, prøver vi igen
+            pass  # Hvis brugeren ikke indtaster et tal, prøv igen
+
+
+def calculate_coins(cents):
+    """Beregner det mindste antal mønter."""
+    coin_values = [25, 10, 5, 1]  # Quarters, dimes, nickels, pennies
+    coins = 0
+
+    for coin in coin_values:
+        coins += cents // coin  # Find ud af hvor mange af denne mønt, der kan bruges
+        cents %= coin           # Reducer cents med værdien af de brugte mønter
+
+    return coins
 
 
 if __name__ == "__main__":
     main()
-
-
