b575f4cf9f72f73ca61bd5e09248d73f1ac98a85
Emilj100 <emilbondo12@gmail.com>
2024-12-03 13:25:00 +0000

    Tue, Dec 3, 2024, 2:25 PM +01:00

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
-x = int(input("x: "))
-y = int(input("y: "))
-
-z = x / y
-print(f"{z:.50f}")
+def get_int(prompt):
+    return int(input(prompt))
 
 
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+
+main()
