3fb59ffb9aee703fe4b4354f16e32c1b12b3eaf5
Emilj100 <emilbondo12@gmail.com>
2024-12-03 18:50:35 +0000

    Tue, Dec 3, 2024, 7:50 PM +01:00

--- cash.py
+++ cash.py
@@ -1,7 +1,7 @@
 
 while True:
     try:
-        dollars = int(input("Change: "))
+        dollars = float(input("Change: "))
         if dollars > 0:
             break
     except ValueError:
@@ -11,20 +11,20 @@ coins = 0
 
 while dollars > 0:
 
-    if dollars >= 25:
+    if dollars >= 0.25:
         coins += 1
-        dollars -= 25
+        dollars -= 0.25
 
-    elif dollars >= 10:
+    elif dollars >= 0.10:
         coins += 1
-        dollars -= 10
+        dollars -= 0.10
 
-    elif dollars >= 5:
+    elif dollars >= 0.05:
         coins += 1
-        dollars -= 5
+        dollars -= 0.05
 
-    elif dollars >= 1:
+    elif dollars >= 0.01:
         coins += 1
-        dollars -= 1
+        dollars -= 0.01
 
 print(coins)
