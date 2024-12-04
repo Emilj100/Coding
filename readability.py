6e48e694bd3b9b1116763131760cf0688e7badee
Emilj100 <emilbondo12@gmail.com>
2024-12-04 13:05:27 +0000

    Wed, Dec 4, 2024, 2:05 PM +01:00

--- readability.py
+++ readability.py
@@ -5,6 +5,7 @@ def main():
     words = count_words(text)
     sentence = count_sentences(text)
 
+    calculate_grade_level(letters, words, sentence)
 
 
 def count_letters(text):
@@ -17,12 +18,18 @@ def count_letters(text):
 def count_words(text):
     counter = 0
     for i in range(len(text)):
-        if text[i.]
-
-
-
+        if text[i].isspace():
+            counter += 1
+    return counter
 
 def count_sentences(text):
+    counter = 0
+    for i in range(len(text)):
+        if text[i] == "?" or text[i] == "." or text[i] == "!":
+            counter += 1
+        return counter
+
+def calculate_grade_level(letters, words, sentence):
 
 
 main()
