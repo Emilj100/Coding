bcf1667052e89dc28a4cf11b94aea722288c5026
Emilj100 <emilbondo12@gmail.com>
2024-12-04 13:02:29 +0000

    Wed, Dec 4, 2024, 2:02 PM +01:00

--- readability.py
+++ readability.py
@@ -2,7 +2,9 @@ def main():
     text = input("Text: ")
 
     letters = count_letters(text)
-    
+    words = count_words(text)
+    sentence = count_sentences(text)
+
 
 
 def count_letters(text):
@@ -11,6 +13,16 @@ def count_letters(text):
         if text[i].isalpha():
             counter += 1
     return counter
+
+def count_words(text):
+    counter = 0
+    for i in range(len(text)):
+        if text[i.]
+
+
+
+
+def count_sentences(text):
 
 
 main()
