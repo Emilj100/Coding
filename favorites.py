84f3714be67efeac0634b9361b07fcd3f2fe12d9
Emilj100 <emilbondo12@gmail.com>
2024-12-05 16:16:07 +0000

    Thu, Dec 5, 2024, 5:16 PM +01:00

--- favorites.py
+++ favorites.py
@@ -2,17 +2,7 @@ import csv
 
 with open("favorites.csv") as file:
     reader = csv.DictReader(file)
-    scratch, c, python = 0, 0, 0
+    counts = {}
 
     for row in reader:
         favorite = row["language"]
-        if favorite == "Scratch":
-            scratch += 1
-        elif favorite == "C":
-            c += 1
-        elif favorite == "Python":
-            python += 1
-
-print(f"Scratch: {scratch}")
-print(f"C: {c}")
-print(f"Python: {python}")
