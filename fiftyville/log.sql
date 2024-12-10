798c069ea93d711020ca70abbbb021dc2d45828d
Emilj100 <emilbondo12@gmail.com>
2024-12-10 19:17:13 +0000

    Tue, Dec 10, 2024, 8:17 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,10 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Find the description of the crime scene
+SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 
+-- Find the interviews for the day the crime happended
+SELECT transcript FROM interviews WHERE month = 7 AND day = 28;
+
+-- Find the car which left the parking lot within 10 minutes (I checked from minute 16-25)
+SELECT activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute = 16-25;
