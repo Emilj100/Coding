dabd2acff52351fc3ea4abd1f923c586bf38e329
Emilj100 <emilbondo12@gmail.com>
2024-12-10 17:19:46 +0000

    Tue, Dec 10, 2024, 6:19 PM +01:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,2 +1,7 @@
-SELECT AVG(rating) from ratings WHERE movie_id IN
-   ...> (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating)
+from ratings
+WHERE movie_id IN (
+   SELECT id
+   FROM movies
+   WHERE year = 2012
+   );
