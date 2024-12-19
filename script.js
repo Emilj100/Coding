34bcec3c7d55c1d6e178ab5cb7ff78705e4a82f3
Emilj100 <emilbondo12@gmail.com>
2024-12-19 18:56:34 +0000

    Thu, Dec 19, 2024, 7:56 PM +01:00

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let div = document.querySelector('#div-project');
-document.querySelector('#show-more').addEventListener('click', function() {
-    div.style.visibility = 'visible';
+document.addEventListener('DOMContentLoaded', function() {
+    let div = document.querySelector('#div-project');
+    document.querySelector('#show-more').addEventListener('click', function() {
+        div.style.visibility = 'visible';
+    });
 });
