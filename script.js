f0c4d763ad19b3cfcd83e95188a3162db36e4789
Emilj100 <emilbondo12@gmail.com>
2024-12-20 12:21:47 +0000

    Fri, Dec 20, 2024, 1:21 PM +01:00

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let div = document.querySelector('#div-project');
     let less = document.querySelector('#show-less');
     let more = document.querySelector('#show-more');
-    document.querySelector('#show-more').addEventListener('click', function() {
-        div.style.visibility = 'visible';
-        less.style.visibility = 'visible';
-        more.style.visibility = 'hidden';
+
+    more.addEventListener('click', function() {
+        div.style.display = 'block';
+        less.style.display = 'inline-block';
+        more.style.display = 'none';
     });
 
-    document.querySelector('#show-less').addEventListener('click', function() {
-        div.style.visibility = 'hidden';
-        less.style.visibility = 'hidden';
-        more.style.visibility = 'visible';
-
+    // Når man klikker på "Show Less"
+    less.addEventListener('click', function() {
+        div.style.display = 'none';
+        less.style.display = 'none';
+        more.style.display = 'inline-block'; 
     });
 });
-
-
