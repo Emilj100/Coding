0fa49d417bfb3e4619725d2d00d855689d63163a
Emilj100 <emilbondo12@gmail.com>
2024-12-22 10:00:27 +0000

    Sun, Dec 22, 2024, 11:00 AM +01:00

--- hello/app.py
+++ hello/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    name = request.args["name"]
+    return render_template("index.html", placeholder=name)
 
 
 
