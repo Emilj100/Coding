cef04a08d6a5786024ef269c43ca53edcf010823
Emilj100 <emilbondo12@gmail.com>
2024-12-23 18:13:53 +0000

    Mon, Dec 23, 2024, 7:13 PM +01:00

--- froshims/app.py
+++ froshims/app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
-SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", sports=SPORTS)
-
-
-@app.route("/register", methods=["POST"])
-def register():
-    if not request.form.get("name"):
-        return render_template("failure.html")
-    for sport in request.form.getlist("sport"):
-        if sport not in SPORTS:
-            return render_template("failure.html")
-
-    return render_template("success.html")
+    return render_template("index.html")
 
 
