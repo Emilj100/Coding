644fa787dcc44b1c068c0e68f037a703df4cd3dc
Emilj100 <emilbondo12@gmail.com>
2024-12-23 18:19:38 +0000

    Mon, Dec 23, 2024, 7:19 PM +01:00

---
+++ Cookies_Sessions/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
--- froshims/app.py
+++ froshims/app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
+SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name"):
+        return render_template("failure.html")
+    for sport in request.form.getlist("sport"):
+        if sport not in SPORTS:
+            return render_template("failure.html")
+
+    return render_template("success.html")
 
 
