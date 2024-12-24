65471fb4c2f0b049eb05af1d3a28423bdb009583
Emilj100 <emilbondo12@gmail.com>
2024-12-24 12:49:46 +0000

    Tue, Dec 24, 2024, 1:49 PM +01:00

---
+++ shopping_cart/app.py
@@ -0,0 +1,15 @@
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///store.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    books = db.execute("SELECT * FROM books")
