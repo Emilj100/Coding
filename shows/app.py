a7e6fc8bccc3fc2e0ed9903ddbcd528c6116d882
Emilj100 <emilbondo12@gmail.com>
2024-12-24 14:00:46 +0000

    Tue, Dec 24, 2024, 3:00 PM +01:00

---
+++ shows/app.py
@@ -0,0 +1,16 @@
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///store.db")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    shows = db.execute("SELECT * FROM show WHERE title = ?")
