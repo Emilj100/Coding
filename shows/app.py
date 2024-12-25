30af6e06b350aa313fe0bb363f7794e1fc382e54
Emilj100 <emilbondo12@gmail.com>
2024-12-25 10:38:27 +0000

    Wed, Dec 25, 2024, 11:38 AM +01:00

--- shows/app.py
+++ shows/app.py
@@ -13,7 +13,8 @@ def index():
 
 @app.route("/search")
 def search():
-    movies = db.execute("SELECT * FROM movies WHERE title LIKE ?", request.args.get("q"))
+    request.args.get("q")
+    movies = db.execute("SELECT * FROM movies WHERE title LIKE ?", "%" + q + "%")
     return render_template("search.html", movies=movies)
 
 
