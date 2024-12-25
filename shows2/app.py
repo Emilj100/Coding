9b4c98a738a33c7f0d3327e1927b2f21a77177b8
Emilj100 <emilbondo12@gmail.com>
2024-12-25 12:12:44 +0000

    Wed, Dec 25, 2024, 1:12 PM +01:00

---
+++ shows2/app.py
@@ -0,0 +1,23 @@
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///movies.db")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+    if q:
+        movies = db.execute("SELECT * FROM movies WHERE title LIKE ? LIMIT 50", "%" + q + "%")
+    else:
+        movies = []
+    return render_template("search.html", shows=shows)
+
+
