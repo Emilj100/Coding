8d9afaa0fd90366752fc35a6799714a4e77042ae
Emilj100 <emilbondo12@gmail.com>
2024-12-29 10:53:34 +0000

    Sun, Dec 29, 2024, 11:53 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -53,8 +53,8 @@ def buy():
         cash = db.execute("SELECT cash FROM users WHERE username = ?", session["user_id"])
         if cash - (shares * symbol) < 0:
             return apology("Not enough money", 408)
-        
 
+        db.execute("INSERT INTO users (username, symbol, shares, price) VALUES (?, ?)", session["user_id"], symbol["symbol"], shares, symbol["price"], )
 
 
         return redirect("/")
