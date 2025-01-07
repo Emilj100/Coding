512894280a1ac8156efa5353900ed0e9cdd9bf39
Emilj100 <emilbondo12@gmail.com>
2025-01-07 09:01:01 +0000

    Tue, Jan 7, 2025, 10:01 AM +01:00

--- project/app.py
+++ project/app.py
@@ -18,7 +18,7 @@ db = SQL("sqlite:///health.db")
 
 def login_required(f):
     """
-    Decorate routes to require login.
+    Decorate routes to require login.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 
     https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
     """
