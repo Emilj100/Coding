48f53d8955ca7fb1d7a3d397b6072ac00fe77fc7
Emilj100 <emilbondo12@gmail.com>
2025-01-07 08:45:24 +0000

    Tue, Jan 7, 2025, 9:45 AM +01:00

--- project/app.py
+++ project/app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
 # Configure application
 app = Flask(__name__)
