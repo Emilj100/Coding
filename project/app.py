96661d4d1b1980c190f6d1d6122a4e8e134c52aa
Emilj100 <emilbondo12@gmail.com>
2025-01-11 09:20:14 +0000

    Sat, Jan 11, 2025, 10:20 AM +01:00

--- project/app.py
+++ project/app.py
@@ -156,6 +156,56 @@ def logout():
 @app.route("/calorietracker")
 @login_required
 def calorietracker():
+
+     try:
+        # Spørger brugeren om hvad de har spist i dag, som vi så indsætter i vores API
+        food_query = input("What did you eat today? ")
+
+
+        API_KEY = "6158963245cf646896228de0c3d0ba3a"
+        APP_ID = "584633a6"
+
+
+        url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+
+
+        headers = {
+            "x-app-id": APP_ID,
+            "x-app-key": API_KEY,
+            "Content-Type": "application/json"
+        }
+
+        # Data, der skal sendes i anmodningen - den tekstbaserede forespørgsel
+        data = {
+            "query": food_query
+        }
+
+        # Send POST-forespørgsel til Nutritionix API
+        response = requests.post(url, headers=headers, json=data)
+
+        # Ændre svaret til JSON-format
+        nutrition_data = response.json()
+
+        # Viser brugeren tallene for det som brugeren har indtastet
+        print("Here is the data for the food you have been eating today:\n")
+
+        all_calories = []
+        all_protein = []
+        all_carbohydrate = []
+        all_fat = []
+
+        for food in nutrition_data["foods"]:
+            all_calories.append(food["nf_calories"])
+            all_protein.append(food["nf_protein"])
+            all_carbohydrate.append(food["nf_total_carbohydrate"])
+            all_fat.append(food["nf_total_fat"])
+
+
+        print(f"{sum(all_calories):.2f} calories")
+        print(f"{sum(all_protein):.2f} protein")
+        print(f"{sum(all_carbohydrate):.2f} carbohydrate")
+        print(f"{sum(all_fat):.2f} fat")
+
     return render_template("calorietracker.html")
 
 @app.route("/traininglog")
