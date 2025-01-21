a6fb30f874595046a88dae512e75e782f399acdc
Emilj100 <emilbondo12@gmail.com>
2025-01-21 17:57:45 +0000

    Tue, Jan 21, 2025, 6:57 PM +01:00

--- test.py
+++ test.py
@@ -1,28 +1,15 @@
 import requests
-import json  # Importer json-modulet for bedre formatering
+import json
 
-food_query = input("What did you eat today? ")
+api_key = "71433d93ff0445e68f984bb19ca3048f"
+url = f"https://api.spoonacular.com/mealplanner/generate?apiKey={api_key}"
 
-API_KEY = "6158963245cf646896228de0c3d0ba3a"
-APP_ID = "584633a6"
-
-url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
-
-headers = {
-    "x-app-id": APP_ID,
-    "x-app-key": API_KEY,
-    "Content-Type": "application/json"
+params = {
+    "timeFrame": "day",  # Kan være 'day' eller 'week'
+    "targetCalories": 2000,  # Brugerens daglige kalorimål
+    "diet": "vegetarian",  # Diætpræference, fx 'vegetarian', 'vegan', osv.
+    "exclude": "nuts, shellfish"  # Ingredienser, der skal undgås
 }
 
-data = {
-    "query": food_query
-}
-
-response = requests.post(url, headers=headers, json=data)
-
-if response.status_code == 200:
-    nutrition_data = response.json()
-    # Gør output menneskevenligt
-    print(json.dumps(nutrition_data, indent=4))
-else:
-    print("Error:", response.status_code, response.text)
+response = requests.get(url, params=params)
+print(response.json())
