99cbf4859fa2d1eeffdb52a51a22f89ea3884432
Emilj100 <emilbondo12@gmail.com>
2025-02-07 14:32:18 +0000

    Fri, Feb 7, 2025, 3:32 PM +01:00

--- project/app.py
+++ project/app.py
@@ -9,10 +9,10 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-# Configure application
+
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
